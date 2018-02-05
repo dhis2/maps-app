9827d1974d5072e33d53f28f330b62f3905c9695
Bjørn Sandvik <bjorn@mastermaps.com>
2018-02-05 11:37:02 +0100

    Color scale fix

--- src/util/colorscale.js
+++ src/util/colorscale.js
@@ -46,6 +46,6 @@ export const getColorScale = (palette) => {
     return colorScales.find(name => colorbrewer[name][classes].join(',') === palette);
 };
 
-export const defaultColorScaleName = 'YlOrRd';
+export const defaultColorScaleName = 'YlOrBr';
 export const defaultClasses = 5;
 export const defaultColorScale = getColorPalette(defaultColorScaleName, defaultClasses);
