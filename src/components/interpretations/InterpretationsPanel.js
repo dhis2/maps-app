19c1292a0c1ee06275ef605a1d2b183ec453cd1d
Bjørn Sandvik <bjorn@mastermaps.com>
2018-06-08 11:52:27 +0200

    Interpretaions logic

--- src/components/interpretations/InterpretationsPanel.js
+++ src/components/interpretations/InterpretationsPanel.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Drawer from 'material-ui/Drawer';
 import Interpretations from '@dhis2/d2-ui-interpretations';
+import queryString from 'query-string';
 import { setRelativePeriodDate } from '../../actions/map';
+import { setInterpretation } from '../../actions/interpretations';
 import {
     HEADER_HEIGHT,
     INTERPRETATIONS_PANEL_WIDTH,
@@ -16,43 +18,50 @@ const style = {
     height: 'auto',
     backgroundColor: '#fafafa',
     boxShadow: '0 3px 10px 0 rgba(0, 0, 0, 0.227451)',
-    // overflowX: 'hidden',
-    // overflowY: 'auto',
+    overflowX: 'hidden',
+    overflowY: 'auto',
     zIndex: 1190,
 };
 
 // https://github.com/EyeSeeTea/d2-ui-playground/blob/feature/interpretations/src/Root.js
-// http://localhost:8082/?id=zDP78aJU8nX&interpretationid=u2ugGSBGTbE
-export const InterpretationsPanel = (
-    { isOpen, mapId, interpretationId, setRelativePeriodDate },
-    { d2 }
-) => {
-    return (
-        <Drawer
-            open={Boolean(isOpen && mapId)}
-            openSecondary={true}
-            containerStyle={style}
-            width={INTERPRETATIONS_PANEL_WIDTH}
-        >
-            {mapId && (
-                <Interpretations
-                    d2={d2}
-                    id={mapId}
-                    type="map"
-                    currentInterpretationId={interpretationId}
-                    onCurrentInterpretationChange={
-                        interpretation =>
-                            setRelativePeriodDate(interpretation.created) // &relativePeriodDate=2018-10-10T18:20:04.272
-                    }
-                />
-            )}
-        </Drawer>
-    );
-};
+class InterpretationsPanel extends Component {
+    static contextTypes = {
+        d2: PropTypes.object,
+    }
 
-InterpretationsPanel.contextTypes = {
-    d2: PropTypes.object,
-};
+    constructor(props, context) {
+        super(props, context);
+        this.props.setInterpretation(queryString.parse(location.search).interpretationid);
+    }
+
+    render() {
+        const { isOpen, mapId,  interpretationId } = this.props;
+
+        return (
+            <Drawer
+                open={Boolean(isOpen && mapId)}
+                openSecondary={true}
+                containerStyle={style}
+                width={INTERPRETATIONS_PANEL_WIDTH}
+            >
+                {mapId && (
+                    <Interpretations
+                        d2={this.context.d2}
+                        id={mapId}
+                        type="map"
+                        currentInterpretationId={interpretationId}
+                        onCurrentInterpretationChange={this.onCurrentInterpretationChange}
+                    />
+                )}
+            </Drawer>
+        )
+    }
+
+    onCurrentInterpretationChange = (interpretation) => {
+        this.props.setInterpretation(interpretation ? interpretation.id : null);
+        this.props.setRelativePeriodDate(interpretation ? interpretation.created : null);
+    }
+}
 
 export default connect(
     state => ({
@@ -60,5 +69,5 @@ export default connect(
         mapId: state.map.id,
         interpretationId: state.interpretation.id,
     }),
-    { setRelativePeriodDate }
+    { setInterpretation, setRelativePeriodDate }
 )(InterpretationsPanel);
