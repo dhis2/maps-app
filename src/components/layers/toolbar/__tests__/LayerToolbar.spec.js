c10889f996874a10a4ccc43682c4cd17762505e4
Austin McGee <austin@dhis2.org>
2018-09-27 13:21:53 +0200

    Factor shallow rendering of LayerToolbar component in tests.

--- src/components/layers/toolbar/__tests__/LayerToolbar.spec.js
+++ src/components/layers/toolbar/__tests__/LayerToolbar.spec.js
@@ -3,104 +3,68 @@ import { shallow } from 'enzyme';
 import { LayerToolbar } from '../LayerToolbar';
 
 describe('LayerToolbar', () => {
-    it('Should render only a visibility toggle and opacity slider', () => {
-        const wrapper = shallow(
+    const shallowRenderLayerToolbar = props => (
+        shallow(
             <LayerToolbar
                 classes={{}}
                 opacity={0}
                 isVisible={true}
                 toggleLayerVisibility={() => null}
                 onOpacityChange={() => null}
+                {...props}
             />
-        );
+        )
+    )
+    it('Should render only a visibility toggle and opacity slider', () => {
+        const wrapper = shallowRenderLayerToolbar();
         expect(wrapper.find('WithStyles(IconButton)').length).toBe(1); // Visibility toggle
         expect(wrapper.find('WithStyles(OpacitySlider)').length).toBe(1);
         expect(wrapper.find('WithStyles(LayerToolbarMoreMenu)').length).toBe(1);
     });
 
     it('Should show VisibilityIcon when visible', () => {
-        const wrapper = shallow(
-            <LayerToolbar
-                classes={{}}
-                opacity={0}
-                isVisible={true}
-                toggleLayerVisibility={() => null}
-                onOpacityChange={() => null}
-            />
-        );
+        const wrapper = shallowRenderLayerToolbar({
+            isVisible: true,
+        });
         expect(wrapper.find('pure(VisibilityIcon)').length).toBe(1);
         expect(wrapper.find('pure(VisibilityOffIcon)').length).toBe(0);
     });
 
     it('Should show VisibilityOffIcon when not visible', () => {
-        const wrapper = shallow(
-            <LayerToolbar
-                classes={{}}
-                opacity={0}
-                isVisible={false}
-                toggleLayerVisibility={() => null}
-                onOpacityChange={() => null}
-            />
-        );
+        const wrapper = shallowRenderLayerToolbar({
+            isVisible: false,
+        });
         expect(wrapper.find('pure(VisibilityIcon)').length).toBe(0);
         expect(wrapper.find('pure(VisibilityOffIcon)').length).toBe(1);
     });
 
     it('Should call toggleLayerVisibility callback on button press', () => {
         const toggleVisibleFn = jest.fn();
-        const wrapper = shallow(
-            <LayerToolbar
-                classes={{}}
-                opacity={0}
-                isVisible={true}
-                toggleLayerVisibility={toggleVisibleFn}
-                onOpacityChange={() => null}
-            />
-        );
+        const wrapper = shallowRenderLayerToolbar({
+            toggleLayerVisibility: toggleVisibleFn,
+        });
         wrapper.find('WithStyles(IconButton)').simulate('click');
         expect(toggleVisibleFn).toHaveBeenCalled();
     });
 
     it('Should match toolbar snapshot without Edit button', () => {
-        const wrapper = shallow(
-            <LayerToolbar
-                classes={{}}
-                opacity={0}
-                isVisible={true}
-                toggleLayerVisibility={() => null}
-                onOpacityChange={() => null}
-            />
-        );
+        const wrapper = shallowRenderLayerToolbar();
         expect(wrapper).toMatchSnapshot();
     });
 
     it('Should render edit button if passed onEdit prop', () => {
-        const wrapper = shallow(
-            <LayerToolbar
-                classes={{}}
-                opacity={0}
-                isVisible={true}
-                toggleLayerVisibility={() => null}
-                onOpacityChange={() => null}
-                onEdit={() => null}
-            />
-        );
+        const wrapper = shallowRenderLayerToolbar({
+            onEdit: () => null,
+        });
         expect(wrapper.find('WithStyles(IconButton)').length).toBe(2); // Visibility toggle and Edit
         expect(wrapper.find('WithStyles(OpacitySlider)').length).toBe(1);
         expect(wrapper.find('WithStyles(LayerToolbarMoreMenu)').length).toBe(1);
     });
 
     it('Should match toolbar snapshot WITH Edit button', () => {
-        const wrapper = shallow(
-            <LayerToolbar
-                classes={{}}
-                opacity={0}
-                isVisible={true}
-                toggleLayerVisibility={() => null}
-                onOpacityChange={() => null}
-                onEdit={() => null}
-            />
-        );
+        const wrapper = shallowRenderLayerToolbar({
+            onEdit: () => null,
+        })
         expect(wrapper).toMatchSnapshot();
     });
 
@@ -108,16 +72,10 @@ describe('LayerToolbar', () => {
         const toggleVisibleFn = jest.fn();
         const editFn = jest.fn();
 
-        const wrapper = shallow(
-            <LayerToolbar
-                classes={{}}
-                opacity={0}
-                isVisible={true}
-                toggleLayerVisibility={toggleVisibleFn}
-                onOpacityChange={() => null}
-                onEdit={editFn}
-            />
-        );
+        const wrapper = shallowRenderLayerToolbar({
+            toggleLayerVisibility: toggleVisibleFn,
+            onEdit: editFn,
+        });
 
         // First button should be Edit
         wrapper
