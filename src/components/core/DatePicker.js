56e3c0ccd90d99d9eac3d88b0278e3c7f1150439
Bjørn Sandvik <bjorn@mastermaps.com>
2018-10-17 14:52:37 +0200

    Correct date format for date picker

--- src/components/core/DatePicker.js
+++ src/components/core/DatePicker.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { timeFormat } from 'd3-time-format';
+
+const formatTime = timeFormat('%Y-%m-%d');
 
 const styles = {
     root: {
@@ -9,13 +12,15 @@ const styles = {
     },
 };
 
+// react-dom.development.js:3337 The specified value "2018-10-17T00:00:00.000" does not conform to the required format, "yyyy-MM-dd".
+
 // DatePicker not yet supported in Material-UI: https://github.com/mui-org/material-ui/issues/4787
 // Fallback on browser native
 const DatePicker = ({ label, value, onChange, classes, style }) => (
     <TextField
         type="date"
         label={label}
-        defaultValue={value}
+        defaultValue={formatTime(new Date(value))}
         onChange={event => onChange(event.target.value)}
         style={style}
         classes={classes}
