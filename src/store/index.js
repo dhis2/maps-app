cd62174bec80e0219a4c562af2566dd61887af2d
Bjørn Sandvik <bjorn@mastermaps.com>
2018-11-06 17:26:44 +0100

    Add development mode check

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,10 @@ import rootEpic from '../epics'; // TODO: Stop using
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer =
+    (process.env.NODE_ENV === 'development' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
 
 const store = createStore(
     reducer,
