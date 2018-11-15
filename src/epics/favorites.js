a403573bc603c9510653aeaf3ef89a1231c9d4bd
Bjorn Sandvik <bjorn@mastermaps.com>
2018-11-15 12:04:50 +0100

    Avoid loosing latest translation and sharing settings (#46)

--- src/epics/favorites.js
+++ src/epics/favorites.js
@@ -15,7 +15,12 @@ export const saveFavorite = (action$, store) =>
             config.mapViews.forEach(view => delete view.id);
         }
 
-        return apiFetch(`/maps/${config.id}`, 'PUT', config).then(() =>
+        // Avoid loosing latest translation and sharing settings
+        return apiFetch(
+            `/maps/${config.id}?skipTranslations=true&skipSharing=true`,
+            'PUT',
+            config
+        ).then(() =>
             setMessage(
                 `${i18n.t('Favorite')} "${config.name}" ${i18n.t('is saved')}.`
             )
@@ -39,28 +44,25 @@ export const saveNewFavorite = (action$, store) =>
                 config.mapViews.forEach(view => delete view.id);
             }
 
-            return apiFetch('/maps/', 'POST', config).then(
-                async (res) => {
-                    const location = res.headers.get('Location');
-                    const id = location ? location.substring(6) : null;
-                    const response = await res.json();
-                    
-                    return (response.status === 'OK' ? { ...config, id } : response)
-                }
-            );
+            return apiFetch('/maps/', 'POST', config).then(async res => {
+                const location = res.headers.get('Location');
+                const id = location ? location.substring(6) : null;
+                const response = await res.json();
+
+                return response.status === 'OK' ? { ...config, id } : response;
+            });
         })
-        .mergeMap(
-            ({ id, name, description, message }) =>
-                name
-                    ? [
-                          setMapProps({ id, name, description }),
-                          setMessage(
-                              `${i18n.t('Favorite')} "${name}" ${i18n.t(
-                                  'is saved'
-                              )}.`
-                          ),
-                      ]
-                    : [setMessage(`${i18n.t('Error')}: ${message}`)]
+        .mergeMap(({ id, name, description, message }) =>
+            name
+                ? [
+                      setMapProps({ id, name, description }),
+                      setMessage(
+                          `${i18n.t('Favorite')} "${name}" ${i18n.t(
+                              'is saved'
+                          )}.`
+                      ),
+                  ]
+                : [setMessage(`${i18n.t('Error')}: ${message}`)]
         );
 
 export default combineEpics(saveFavorite, saveNewFavorite);
