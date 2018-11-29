dbac39e18ca31f2b527be549419a9353a6be569e
Austin McGee <austin@dhis2.org>
2018-11-29 12:26:20 +0100

    Remove Enzyme mount

--- src/components/errors/__tests__/FatalErrorBoundary.spec.js
+++ src/components/errors/__tests__/FatalErrorBoundary.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FatalErrorBoundary } from '../FatalErrorBoundary';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 const Something = () => {
     // Placeholder
