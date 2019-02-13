9dc9eb1389564d430e40b8c176b1678958c6f6e4
Bjørn Sandvik <bjorn@mastermaps.com>
2019-02-13 17:10:51 +0100

    Map engine change

--- src/components/map/MapProvider.js
+++ src/components/map/MapProvider.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-//import D2map from '@dhis2/gis-api';
+// import D2map from '@dhis2/gis-api';
 // import D2map from '@dhis2/gis-api/src';
 import D2map from '@dhis2/maps-gl';
 
