d60a7b67f776cfb04b971ebd2869f1e568bd804f
Bjørn Sandvik <bjorn@mastermaps.com>
2019-02-14 14:31:48 +0100

    fix/DHIS2-6003

--- src/epics/orgUnits.js
+++ src/epics/orgUnits.js
@@ -53,7 +53,7 @@ export const loadOrgUnitLevels = action$ =>
             .then(async d2 =>
                 d2.models.organisationUnitLevels.list({
                     fields: `id,${getDisplayPropertyUrl(d2)},level`,
-                    pageing: false,
+                    paging: false,
                 })
             )
             .then(levels => setOrgUnitLevels(levels.toArray()))
@@ -68,7 +68,7 @@ export const loadOrgUnitGroups = action$ =>
             .then(async d2 =>
                 d2.models.organisationUnitGroups.list({
                     fields: `id,${getDisplayPropertyUrl(d2)}`,
-                    pageing: false,
+                    paging: false,
                 })
             )
             .then(groups => setOrgUnitGroups(groups.toArray()))
@@ -83,7 +83,7 @@ export const loadOrgUnitGroupSets = action$ =>
             .then(d2 =>
                 d2.models.organisationUnitGroupSets.list({
                     fields: `id,${getDisplayPropertyUrl(d2)}`,
-                    pageing: false,
+                    paging: false,
                 })
             )
             .then(groupSets => setOrgUnitGroupSets(groupSets.toArray()))
