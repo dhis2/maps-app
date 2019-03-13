6f425e5e973eaa2422291d596eabc41444f3e507
Bjørn Sandvik <bjorn@mastermaps.com>
2019-03-13 18:15:33 +0100

    Layer order fix for external layers

--- src/components/map/ExternalLayer.js
+++ src/components/map/ExternalLayer.js
@@ -2,13 +2,17 @@ import Layer from './Layer';
 
 export default class ExternalLayer extends Layer {
     createLayer() {
-        const props = this.props;
-        const config = props.config;
-        const map = this.context.map;
+        const { id, index, opacity, isVisible, config } = this.props;
+        const { map } = this.context;
 
-        config.id = props.id;
-        config.index = props.index;
+        this.layer = map.createLayer({
+            id,
+            index,
+            opacity,
+            isVisible,
+            ...config,
+        });
 
-        this.layer = map.createLayer(config);
+        map.addLayer(this.layer);
     }
 }
