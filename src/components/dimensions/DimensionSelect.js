2551acfb27e5e5045262f5ed3b03b53e29a17293
Bjørn Sandvik <bjorn@mastermaps.com>
2019-04-04 16:23:22 +0200

    Dimension select

--- src/components/dimensions/DimensionSelect.js
+++ src/components/dimensions/DimensionSelect.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import i18n from '@dhis2/d2-i18n';
-import SelectField from '../core/SelectField';
+import Popover from '@material-ui/core/Popover';
+import TextField from '@material-ui/core/TextField';
+// import SelectField from '../core/SelectField';
+import { DimensionsPanel } from '@dhis2/d2-ui-analytics';
 import { loadDimensions } from '../../actions/dimensions';
 
+// Inspired by: https://github.com/dhis2/dashboards-app/blob/feat/period-filter/src/components/ItemFilter/FilterSelector.js
 export class DimensionSelect extends Component {
     static propTypes = {
         dimension: PropTypes.string,
-        dimensions: PropTypes.array,
+        dimensions: PropTypes.object,
         onChange: PropTypes.func.isRequired,
         loadDimensions: PropTypes.func.isRequired,
         style: PropTypes.object,
@@ -26,6 +30,41 @@ export class DimensionSelect extends Component {
     render() {
         const { dimension, dimensions, onChange } = this.props;
 
+        if (!dimensions) {
+            return null;
+        }
+
+        console.log('dimensions', dimensions);
+
+        return (
+            <Fragment>
+                <TextField
+                    label={i18n.t('Dimension')}
+                    // className={classes.textField}
+                    // value={this.state.currency}
+                    // onChange={console.log}
+                    // helperText="Please select your currency"
+                    // margin="normal"
+                    onClick={console.log}
+                    style={{ width: 200 }}
+                />
+            </Fragment>
+        );
+
+        /*
+
+                <Popover
+                    open={true}
+                    onClose={console.log}
+                    style={{ height: '100%' }}
+                >
+                    <DimensionsPanel
+                        dimensions={dimensions}
+                        onDimensionClick={console.log}
+                        selectedIds={['uIuxlbV1vRT']}
+                    />
+                </Popover>
+
         return (
             <SelectField
                 label={i18n.t('Dimension')}
@@ -39,12 +78,18 @@ export class DimensionSelect extends Component {
                 style={{ width: 200 }}
             />
         );
+        */
     }
 }
 
 export default connect(
-    state => ({
-        dimensions: state.dimensions,
+    ({ dimensions }) => ({
+        dimensions: dimensions
+            ? dimensions.reduce((obj, dim) => {
+                  obj[dim.id] = dim;
+                  return obj;
+              }, {})
+            : null,
     }),
     { loadDimensions }
 )(DimensionSelect);
