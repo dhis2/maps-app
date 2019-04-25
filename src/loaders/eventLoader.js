42a764a95b8d91830209a6b5a26128a6cd1acd8e
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-04-25 14:41:34 +0200

    fix: show an alert when a load error occurs

--- src/loaders/eventLoader.js
+++ src/loaders/eventLoader.js
@@ -21,10 +21,37 @@ import { cssColor } from '../util/colors';
 // Server clustering if more than 2000 events
 const useServerCluster = count => count > 2000; // TODO: Use constant
 
+const accessDeniedAlert = createAlert(
+    i18n.t('Access denied'),
+    i18n.t('user is not allowed to read layer data')
+);
+const unknownErrorAlert = createAlert(
+    i18n.t('Failed'),
+    i18n.t('an unknown error occurred while reading layer data')
+);
+
 // TODO: Refactor to share code with other loaders
 // Returns a promise
 const eventLoader = async layerConfig => {
-    const config = { ...layerConfig };
+    let config = { ...layerConfig };
+    try {
+        await loadEventLayer(config);
+    } catch (e) {
+        if (e.httpStatusCode === 403 || e.httpStatusCode === 409) {
+            config.alerts = [accessDeniedAlert];
+        } else {
+            config.alerts = [unknownErrorAlert];
+        }
+    }
+
+    config.isLoaded = true;
+    config.isExpanded = true;
+    config.isVisible = true;
+
+    return config;
+};
+
+const loadEventLayer = async config => {
     const {
         columns,
         endDate,
@@ -104,12 +131,6 @@ const eventLoader = async layerConfig => {
     if (alert) {
         config.alerts = [alert];
     }
-
-    config.isLoaded = true;
-    config.isExpanded = true;
-    config.isVisible = true;
-
-    return config;
 };
 
 // Also used to query for server cluster in map/EventLayer.js
