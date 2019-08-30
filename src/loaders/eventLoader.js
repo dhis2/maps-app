2f560cc945fac2852fea1275e5d13f75a6bc15aa
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-08-30 13:49:23 +0200

    fix: (v30) show an alert when a load error occurs (#161)
    * fix: (v30) show an alert when a load error occurs
    
    * fix: update backport to work with non-destructive logic, retain name

--- src/loaders/eventLoader.js
+++ src/loaders/eventLoader.js
@@ -15,13 +15,46 @@ import {
 } from '../util/analytics';
 import { EVENT_COLOR, EVENT_RADIUS } from '../constants/layers';
 import { defaultClasses, defaultColorScale } from '../util/colorscale';
+import { createAlert } from '../util/alerts';
 
 // Look at: https://github.com/dhis2/maintenance-app/blob/master/src/App/appStateStore.js
 
 const formatTime = date => timeFormat('%Y-%m-%d')(new Date(date));
 
-const eventLoader = async config => {
-    // Returns a promise
+const accessDeniedAlert = createAlert(
+    i18n.t('Access denied'),
+    i18n.t('user is not allowed to read layer data')
+);
+const unknownErrorAlert = createAlert(
+    i18n.t('Failed'),
+    i18n.t('an unknown error occurred while reading layer data')
+);
+
+// TODO: Refactor to share code with other loaders
+// Returns a promise
+const eventLoader = async layerConfig => {
+    let config = {
+        ...layerConfig,
+        name: layerConfig.programStage && layerConfig.programStage.name
+    };
+    try {
+        config = await loadEventLayer(config);
+    } catch (e) {
+        if (e.httpStatusCode === 403 || e.httpStatusCode === 409) {
+            config.alerts = [accessDeniedAlert];
+        } else {
+            config.alerts = [unknownErrorAlert];
+        }
+    }
+
+    config.isLoaded = true;
+    config.isExpanded = true;
+    config.isVisible = true;
+
+    return config;
+};
+
+const loadEventLayer = async config => {
     const {
         classes = defaultClasses,
         colorScale = defaultColorScale,
@@ -157,7 +190,7 @@ const eventLoader = async config => {
                     data.forEach(feature => {
                         feature.properties.color =
                             styleDataItem.optionSet.options[
-                                feature.properties.value
+                            feature.properties.value
                             ];
                     });
                     legend.items = getCategoryLegendItems(
@@ -196,14 +229,10 @@ const eventLoader = async config => {
 
     return {
         ...config,
-        name: programStage.name,
         legend,
         data,
         bounds,
         serverCluster,
-        isLoaded: true,
-        isExpanded: true,
-        isVisible: true,
     };
 };
 
@@ -277,12 +306,12 @@ export const getAnalyticsRequest = async (
 export const addStyleDataItem = (dataItems, styleDataItem) =>
     styleDataItem
         ? [
-              ...dataItems,
-              styleDataItem && {
-                  dimension: styleDataItem.id,
-                  name: styleDataItem.name,
-              },
-          ]
+            ...dataItems,
+            styleDataItem && {
+                dimension: styleDataItem.id,
+                name: styleDataItem.name,
+            },
+        ]
         : [...dataItems];
 
 const createEventFeature = (headers, names, event, eventCoordinateField) => {
