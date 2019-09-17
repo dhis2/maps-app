e82ebbfb5dcdeb8e1181f4a67e7c84ba67fc868d
Bjørn Sandvik <bjorn@mastermaps.com>
2019-09-17 16:52:22 +0200

    Map loading mask

---
+++ src/components/map/MapLoadingMask.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = () => ({
+    mask: {
+        zIndex: 990,
+        backgroundColor: 'rgba(255,255,255,0.7)',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+const MapLoadingMask = ({ classes }) => {
+    return (
+        <div className={classes.mask}>
+            <CircularProgress size={32} />
+        </div>
+    );
+};
+
+MapLoadingMask.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MapLoadingMask);
