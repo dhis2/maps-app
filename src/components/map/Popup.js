d027926f41b99ba7656cac26c99a71a513ca8549
Bjørn Sandvik <bjorn@mastermaps.com>
2019-11-25 11:59:19 +0100

    Dependenciy array for Popup component

--- src/components/map/Popup.js
+++ src/components/map/Popup.js
@@ -3,16 +3,18 @@ import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import './Popup.css';
 
+// Displ
 const Popup = (props, context) => {
     const { className = '', coordinates, onClose, children } = props;
     const { map } = context;
     const container = useMemo(() => document.createElement('div'), []);
 
+    // Create and open popup on map
     useEffect(() => {
         container.className = className;
         map.openPopup(container, coordinates, onClose);
         return () => map.closePopup();
-    });
+    }, [map, container, className, coordinates, onClose]);
 
     return createPortal(children, container);
 };
