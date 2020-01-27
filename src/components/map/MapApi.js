742fb7c0a0856566ddf36ec8892374e8fdeefbf8
Bjorn Sandvik <bjorn@dhis2.org>
2020-01-27 18:02:37 +0100

    feat: mapping interface locale (#412)
    * Use GIS API
    
    * Refactor map locale strings
    
    * Text change
    
    * Code clean
    
    * Add locale string
    
    * Map locale refactor
    
    * Generate translation strings
    
    * Code fix
    
    * Map locales in separate file

--- src/components/map/MapApi.js
+++ src/components/map/MapApi.js
@@ -1,12 +1,18 @@
 import MapApi, { layerTypes, controlTypes } from '@dhis2/gis-api';
 // import MapApi, { layerTypes, controlTypes } from '@dhis2/maps-gl';
+import getMapLocale from './mapLocale';
 
 // Returns a new map instance
 const map = options => {
     const div = document.createElement('div');
+
     div.style.width = '100%';
     div.style.height = '100%';
-    return new MapApi(div, options);
+
+    return new MapApi(div, {
+        ...options,
+        locale: getMapLocale(),
+    });
 };
 
 export { layerTypes, controlTypes };
