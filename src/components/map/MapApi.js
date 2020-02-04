d29a2f9144a42bdccd00daa8a7baf464afe6b594
Bjørn Sandvik <bjorn@mastermaps.com>
2020-02-04 19:53:01 +0100

    fix: revert back to gis-api

--- src/components/map/MapApi.js
+++ src/components/map/MapApi.js
@@ -2,7 +2,7 @@ import MapApi, {
     layerTypes,
     controlTypes,
     loadEarthEngineApi,
-} from '@dhis2/maps-gl';
+} from '@dhis2/gis-api';
 import getMapLocale from './mapLocale';
 
 // Returns a new map instance
