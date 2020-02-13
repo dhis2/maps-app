6bc1ce26b4ee161e821f63600116d0a17987b506
Bjørn Sandvik <bjorn@mastermaps.com>
2020-02-13 14:42:22 +0100

    chore: refactor plugin context menu to use hooks

--- src/components/plugin/ContextMenu.js
+++ src/components/plugin/ContextMenu.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import i18n from '@dhis2/d2-i18n';
 import './ContextMenu.css';
 
-class ContextMenu extends Component {
-    componentDidUpdate() {
-        const { position } = this.props;
+const ContextMenu = props => {
+    const { position, feature, onDrill } = props;
+    const menuEl = useRef();
 
-        if (position) {
-            this.el.style.left = position[0] + 'px';
-            this.el.style.top = position[1] + 'px';
+    useEffect(() => {
+        if (menuEl && position) {
+            const { style } = menuEl.current;
+            const [x, y] = position;
+
+            style.left = `${x}px`;
+            style.top = `${y}px`;
         }
+    }, [menuEl, position]);
+
+    if (!position || !feature) {
+        return null;
     }
 
-    render() {
-        const { position, feature, onDrill } = this.props;
+    const { hasCoordinatesUp, hasCoordinatesDown } = feature.properties;
 
-        if (!position || !feature) {
-            return null;
-        }
-
-        const { hasCoordinatesUp, hasCoordinatesDown } = feature.properties;
-
-        return (
-            <div className="dhis2-map-context-menu" ref={el => (this.el = el)}>
-                {hasCoordinatesUp && (
-                    <div onClick={() => onDrill('up')}>
-                        {i18n.t('Drill up one level')}
-                    </div>
-                )}
-                {hasCoordinatesDown && (
-                    <div onClick={() => onDrill('down')}>
-                        {i18n.t('Drill down one level')}
-                    </div>
-                )}
-            </div>
-        );
-    }
-}
+    return (
+        <div className="dhis2-map-context-menu" ref={menuEl}>
+            {hasCoordinatesUp && (
+                <div onClick={() => onDrill('up')}>
+                    {i18n.t('Drill up one level')}
+                </div>
+            )}
+            {hasCoordinatesDown && (
+                <div onClick={() => onDrill('down')}>
+                    {i18n.t('Drill down one level')}
+                </div>
+            )}
+        </div>
+    );
+};
 
 ContextMenu.propTypes = {
     feature: PropTypes.object,
