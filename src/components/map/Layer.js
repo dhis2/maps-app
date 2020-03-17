8ec724e03bc7add0058eed2ce7fdcdbf27dbdd61
Bjorn Sandvik <bjorn@dhis2.org>
2020-03-17 20:21:30 +0100

    fix: Handle error if org unit selection is invalid for thematic layers (DHIS2-8479) (#496)
    * fix: Handle error if org unit selection is invalid for thematic layers (DHIS2-8479)
    
    * chore: remove redundant code

--- src/components/map/Layer.js
+++ src/components/map/Layer.js
@@ -27,6 +27,8 @@ class Layer extends PureComponent {
         isVisible: true,
     };
 
+    state = {};
+
     constructor(...args) {
         super(...args);
         this.setPeriod();
@@ -43,7 +45,7 @@ class Layer extends PureComponent {
             editCounter,
             dataFilters,
         } = this.props;
-        const { period } = this.state || {};
+        const { period } = this.state;
         const { period: prevPeriod } = prevState || {};
         const isEdited = editCounter !== prevProps.editCounter;
 
