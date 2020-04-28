e8bd5630adda2d4ab3cf295fda28865853a80c1c
Bjørn Sandvik <bjorn@mastermaps.com>
2020-04-28 14:16:39 +0200

    fix: DHIS2-8575 backport

--- src/util/analyticalObject.js
+++ src/util/analyticalObject.js
@@ -1,5 +1,5 @@
 import { config, getInstance as getD2 } from 'd2';
-import { getPeriodNameFromId } from './analytics';
+import { getPeriodNameFromId, getDimensionsFromFilters } from './analytics';
 import { loadDataItemLegendSet } from './legend';
 
 export const NAMESPACE = 'analytics';
@@ -41,6 +41,7 @@ export const getThematicLayerFromAnalyticalObject = async (
     const dataDims = getDataDimensionsFromAnalyticalObject(ao);
     const dims = getDimensionsFromAnalyticalObject(ao);
     const orgUnits = dims.find(i => i.dimension === 'ou');
+    const filters = getDimensionsFromFilters(dims); // Dynamic dimension filters
     let period = dims.find(i => i.dimension === 'pe');
     let dataDim = dataDims[0];
 
@@ -72,7 +73,7 @@ export const getThematicLayerFromAnalyticalObject = async (
         layer: 'thematic',
         columns: [{ dimension: 'dx', items: [dataDim] }],
         rows: [orgUnits],
-        filters: [period],
+        filters: [period, ...filters],
         aggregationType,
         legendSet,
         isVisible,
