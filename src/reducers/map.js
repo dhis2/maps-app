956df64150076711a47736352c4fba30a5c9c326
Bjørn Sandvik <bjorn@mastermaps.com>
2020-05-05 13:18:07 +0200

    chore: array-move dependency

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -1,5 +1,5 @@
 import * as types from '../constants/actionTypes';
-import { arrayMove } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
 import { generateUid } from 'd2/uid';
 
 const defaultState = {
