7fa10f746329ae69d06cb0dc49bcca868fb7bfbb
Bjørn Sandvik <bjorn@mastermaps.com>
2020-10-22 15:30:13 +0200

    chore: code cleaning

--- src/components/layers/layers/AddLayerButton.js
+++ src/components/layers/layers/AddLayerButton.js
@@ -1,42 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
 import i18n from '@dhis2/d2-i18n';
 import { DropdownButton } from '@dhis2/ui';
 import AddIcon from '@material-ui/icons/AddCircleOutline';
 import AddLayerPopover from './AddLayerPopover';
-import { openLayersDialog } from '../../../actions/layers';
+import styles from './styles/DownloadDialog.module.css';
 
-export class AddLayerButton extends Component {
-    static propTypes = {
-        openLayersDialog: PropTypes.func.isRequired,
-    };
+const AddLayerButton = () => (
+    <div className={styles.addLayerBtn}>
+        <DropdownButton
+            secondary
+            component={<AddLayerPopover />}
+            icon={<AddIcon />}
+            dataTest="addlayerbutton"
+        >
+            {i18n.t('Add layer')}
+        </DropdownButton>
+    </div>
+);
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    handleClick = event => {
-        event.preventDefault(); // This prevents ghost click.
-        this.setState({
-            anchorEl: event.currentTarget,
-        });
-        this.props.openLayersDialog();
-    };
-
-    render() {
-        return (
-            <DropdownButton
-                secondary
-                component={<AddLayerPopover />}
-                icon={<AddIcon />}
-                dataTest="addlayerbutton"
-            >
-                {i18n.t('Add layer')}
-            </DropdownButton>
-        );
-    }
-}
-
-export default connect(null, { openLayersDialog })(AddLayerButton);
+export default AddLayerButton;
