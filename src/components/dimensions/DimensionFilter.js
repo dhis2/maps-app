77ae5efeaa6ff1126276a5a58e21f6d163a953dc
Bjørn Sandvik <bjorn@mastermaps.com>
2020-11-03 10:55:53 +0100

    chore: dimension filter refactor

--- src/components/dimensions/DimensionFilter.js
+++ src/components/dimensions/DimensionFilter.js
@@ -1,72 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
 import i18n from '@dhis2/d2-i18n';
-import { Button } from '@material-ui/core';
+import { Button } from '@dhis2/ui';
 import DimensionFilterRow from './DimensionFilterRow';
 import {
     addDimensionFilter,
     removeDimensionFilter,
     changeDimensionFilter,
 } from '../../actions/layerEdit';
+import styles from './styles/DimensionFilter.module.css';
 
-const styles = () => ({
-    container: {
-        width: '100%',
-        height: 300,
-        paddingTop: 8,
-        overflowY: 'auto',
-    },
-    button: {
-        marginTop: 8,
-    },
-});
+const DimensionFilter = ({
+    dimensions = [],
+    addDimensionFilter,
+    changeDimensionFilter,
+    removeDimensionFilter,
+}) => (
+    <div className={styles.dimensionFilter}>
+        {dimensions.map((item, index) => (
+            <DimensionFilterRow
+                key={index}
+                index={index}
+                onChange={changeDimensionFilter}
+                onRemove={removeDimensionFilter}
+                {...item}
+            />
+        ))}
+        <div className={styles.addFilter}>
+            <Button primary onClick={() => addDimensionFilter()}>
+                {i18n.t('Add filter')}
+            </Button>
+        </div>
+    </div>
+);
 
-class DimensionFilter extends Component {
-    static propTypes = {
-        dimensions: PropTypes.array,
-        addDimensionFilter: PropTypes.func.isRequired,
-        removeDimensionFilter: PropTypes.func.isRequired,
-        changeDimensionFilter: PropTypes.func.isRequired,
-        classes: PropTypes.object.isRequired,
-    };
-
-    render() {
-        const {
-            addDimensionFilter,
-            classes,
-            dimensions = [],
-            changeDimensionFilter,
-            removeDimensionFilter,
-        } = this.props;
-
-        return (
-            <div className={classes.container}>
-                {dimensions.map((item, index) => (
-                    <DimensionFilterRow
-                        key={index}
-                        index={index}
-                        onChange={changeDimensionFilter}
-                        onRemove={removeDimensionFilter}
-                        {...item}
-                    />
-                ))}
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => addDimensionFilter()}
-                    className={classes.button}
-                >
-                    {i18n.t('Add filter')}
-                </Button>
-            </div>
-        );
-    }
-}
+DimensionFilter.propTypes = {
+    dimensions: PropTypes.array,
+    addDimensionFilter: PropTypes.func.isRequired,
+    removeDimensionFilter: PropTypes.func.isRequired,
+    changeDimensionFilter: PropTypes.func.isRequired,
+};
 
 export default connect(null, {
     addDimensionFilter,
     removeDimensionFilter,
     changeDimensionFilter,
-})(withStyles(styles)(DimensionFilter));
+})(DimensionFilter);
