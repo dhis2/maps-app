34efc3cc04d7f89ab2b2c9dd4341570a6e6574e3
Bjørn Sandvik <bjorn@mastermaps.com>
2020-11-03 17:41:44 +0100

    chore: dimension filter refactor

--- src/components/filter/RemoveFilter.js
+++ src/components/filter/RemoveFilter.js
@@ -1,49 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import i18n from '@dhis2/d2-i18n';
-import { withStyles } from '@material-ui/core/styles';
-import { IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { Tooltip } from '@material-ui/core';
-
-const styles = theme => ({
-    removeBtnContainer: {
-        borderLeft: `1px solid ${theme.palette.divider}`,
-        cursor: 'pointer',
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        right: 0,
-        padding: '10px 0px',
-        '&:hover': {
-            backgroundColor: theme.palette.primary.lightest,
-        },
-    },
-    removeBtn: {
-        color: theme.palette.status.negative,
-        '&:hover': {
-            backgroundColor: 'inherit',
-        },
-    },
-});
+import { Tooltip } from '@dhis2/ui';
+import styles from './styles/RemoveFilter.module.css';
 
 // Remove filter button used for both thematic and event filters
-const RemoveFilter = ({ onClick, classes }) => (
-    <Tooltip title={i18n.t('Remove filter')}>
-        <div className={classes.removeBtnContainer} onClick={onClick}>
-            <IconButton
-                tooltip={i18n.t('Remove filter')}
-                className={classes.removeBtn}
-            >
+const RemoveFilter = ({ onClick }) => (
+    <Tooltip content={i18n.t('Remove filter')}>
+        <div className={styles.removeBtnContainer} onClick={onClick}>
+            <div className={styles.removeBtn}>
                 <DeleteIcon />
-            </IconButton>
+            </div>
         </div>
     </Tooltip>
 );
 
 RemoveFilter.propTypes = {
     onClick: PropTypes.func.isRequired,
-    classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(RemoveFilter);
+export default RemoveFilter;
