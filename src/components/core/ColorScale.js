d5786aa5983988e27eb57cd15322eb84433b429c
Bjørn Sandvik <bjorn@mastermaps.com>
2020-11-04 18:21:47 +0100

    chore: color scale select refactor

--- src/components/core/ColorScale.js
+++ src/components/core/ColorScale.js
@@ -1,43 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import cx from 'classnames';
 import colorbrewer from '../../constants/colorbrewer';
-
-const styles = {
-    scale: {
-        margin: '8px 0 0 0',
-        paddingLeft: 0,
-        height: 36,
-        cursor: 'pointer',
-        boxShadow: '0 1px 6px rgba(0,0,0,0.12),0 1px 4px rgba(0,0,0,0.12)',
-        display: 'inline-block',
-        whiteSpace: 'nowrap',
-    },
-    item: {
-        marginLeft: 0,
-        display: 'inline-block',
-        height: '100%',
-    },
-};
+import styles from './styles/ColorScale.module.css';
 
 // Returns one color scale based on a code and number of classes
-const ColorScale = ({ scale, bins, width, style, onClick, classes }) => {
+const ColorScale = ({ scale, bins, width, className, onClick }) => {
     const colors = colorbrewer[scale][bins];
     const itemWidth = width ? width / bins : 36;
 
     return (
         <ul
-            className={classes.scale}
+            onClick={() => onClick(scale)}
+            className={cx(styles.colorScale, className)}
             style={{
                 ...(width && { width }),
-                ...style,
             }}
-            onClick={event => onClick(event, scale)}
         >
             {colors.map((color, index) => (
                 <li
                     key={index}
-                    className={classes.item}
+                    className={styles.item}
                     style={{ backgroundColor: color, width: itemWidth }}
                 />
             ))}
@@ -50,8 +33,7 @@ ColorScale.propTypes = {
     scale: PropTypes.string.isRequired,
     width: PropTypes.number,
     onClick: PropTypes.func.isRequired,
-    style: PropTypes.object,
-    classes: PropTypes.object.isRequired,
+    className: PropTypes.string,
 };
 
-export default withStyles(styles)(ColorScale);
+export default ColorScale;
--- src/components/core/ColorScaleSelect.js
+++ src/components/core/ColorScaleSelect.js
@@ -1,31 +1,69 @@
-import React, { Component } from 'react';
+import React, { Fragment, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { Popover } from '@material-ui/core';
+import { Popover } from '@dhis2/ui';
+import cx from 'classnames';
 import ColorScale from './ColorScale';
 import { colorScales, getColorScale, getColorPalette } from '../../util/colors';
+import styles from './styles/ColorScaleSelect.module.css';
 
-const styles = {
-    scale: {
-        marginTop: 8,
-        overflow: 'visible',
-        whiteSpace: 'nowrap',
-    },
-    scaleItem: {
-        display: 'block',
-        margin: '5px 12px 0 12px',
-        overflow: 'visible',
-        whiteSpace: 'nowrap',
-    },
+const ColorScaleSelect = ({ palette, width, onChange, className }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const anchorRef = useRef();
+
+    const bins = palette.split(',').length;
+    const scale = getColorScale(palette);
+
+    const onColorScaleSelect = scale => {
+        const classes = palette.split(',').length;
+        onChange(getColorPalette(scale, classes));
+        setIsOpen(false);
+    };
+
+    return (
+        <Fragment>
+            <div ref={anchorRef} className={cx(styles.colorScale, className)}>
+                <ColorScale
+                    bins={bins}
+                    scale={scale}
+                    onClick={() => setIsOpen(true)}
+                    width={width || 260}
+                />
+            </div>
+            {isOpen && (
+                <Popover
+                    reference={anchorRef}
+                    arrow={false}
+                    placement="right"
+                    onClickOutside={() => setIsOpen(false)}
+                >
+                    <div
+                        className={styles.popover}
+                        style={{ width: width + 24 || 260 }}
+                    >
+                        {colorScales.map((scale, index) => (
+                            <ColorScale
+                                key={index}
+                                scale={scale}
+                                bins={bins}
+                                style={styles.scaleItem}
+                                onClick={onColorScaleSelect}
+                                width={width || 260}
+                            />
+                        ))}
+                    </div>
+                </Popover>
+            )}
+        </Fragment>
+    );
 };
 
+/*
 class ColorScaleSelect extends Component {
     static propTypes = {
         palette: PropTypes.string.isRequired,
         onChange: PropTypes.func.isRequired,
         width: PropTypes.number,
         style: PropTypes.object,
-        classes: PropTypes.object.isRequired,
     };
 
     constructor(...args) {
@@ -98,5 +136,13 @@ class ColorScaleSelect extends Component {
         );
     }
 }
+*/
 
-export default withStyles(styles)(ColorScaleSelect);
+ColorScaleSelect.propTypes = {
+    palette: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    width: PropTypes.number,
+    className: PropTypes.string,
+};
+
+export default ColorScaleSelect;
