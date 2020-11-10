c3689991c9bccc737390ab3b4dc056b3624df4de
Bjørn Sandvik <bjorn@mastermaps.com>
2020-11-10 21:55:45 +0100

    chore: code clean

--- src/components/layers/toolbar/OpacitySlider.js
+++ src/components/layers/toolbar/OpacitySlider.js
@@ -3,21 +3,6 @@ import PropTypes from 'prop-types';
 import { Slider } from '@material-ui/core';
 import styles from './styles/OpacitySlider.module.css';
 
-/*
-const styles = theme => ({
-    root: {
-        width: 100,
-        background: 'yellow',
-    },
-    track: {
-        backgroundColor: theme.palette.action.active,
-    },
-    thumb: {
-        backgroundColor: theme.palette.action.active,
-    },
-});
-*/
-
 const OpacitySlider = ({ opacity, onChange }) => (
     <div className={styles.slider}>
         <Slider
@@ -26,7 +11,6 @@ const OpacitySlider = ({ opacity, onChange }) => (
             max={1}
             step={0.01}
             onChange={(evt, opacity) => onChange(opacity)}
-            // classes={classes}
         />
     </div>
 );
