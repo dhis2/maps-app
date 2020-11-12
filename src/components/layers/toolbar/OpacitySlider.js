3861db24286b2a07ae3953eb9c672f934664347b
Bjørn Sandvik <bjorn@mastermaps.com>
2020-11-12 15:44:30 +0100

    chore: slider refactor

--- src/components/layers/toolbar/OpacitySlider.js
+++ src/components/layers/toolbar/OpacitySlider.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { Slider } from '@material-ui/core';
 import styles from './styles/OpacitySlider.module.css';
 
-const OpacitySlider = ({ opacity, onChange }) => (
-    <div className={styles.slider}>
-        <Slider
-            value={opacity}
-            min={0}
-            max={1}
-            step={0.01}
-            onChange={(evt, opacity) => onChange(opacity)}
-        />
-    </div>
-);
+const OpacitySlider = ({ opacity, onChange }) => {
+    const onSliderChange = useCallback(
+        evt => onChange(Number(evt.target.value)),
+        [onChange]
+    );
+
+    return (
+        <div className={styles.container}>
+            <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                value={opacity}
+                onChange={onSliderChange}
+                className={styles.slider}
+            />
+        </div>
+    );
+};
 
 OpacitySlider.propTypes = {
     opacity: PropTypes.number.isRequired,
