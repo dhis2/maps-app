22cabfbcac9af314c3bc8a462e9b5a8d44ce2738
Bjørn Sandvik <bjorn@mastermaps.com>
2020-11-17 16:47:16 +0100

    chore: thematic map type select

--- src/components/edit/thematic/ThematicMapTypeSelect.js
+++ src/components/edit/thematic/ThematicMapTypeSelect.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import RadioGroup from '../../core/RadioGroup';
-import Radio from '../../core/Radio';
+import ImageSelect from '../../core/ImageSelect';
 import { setThematicMapType } from '../../../actions/layerEdit';
 import {
     THEMATIC_CHOROPLETH,
     getThematicMapTypes,
 } from '../../../constants/layers';
+import styles from '../styles/LayerDialog.module.css';
 
 // Select between choropleth and bubble map for thematic layers
 export const ThematicMapTypeSelect = ({
     type = THEMATIC_CHOROPLETH,
     setThematicMapType,
 }) => (
-    <RadioGroup name="type" value={type} onChange={setThematicMapType}>
-        {getThematicMapTypes().map(({ id, name }) => (
-            <Radio key={id} value={id} label={name} />
+    <div className={styles.flexInnerColumnFlow}>
+        {getThematicMapTypes().map(({ id, name, image }) => (
+            <ImageSelect
+                key={id}
+                id={id}
+                img={image}
+                title={name}
+                onClick={setThematicMapType}
+                isSelected={type === id}
+                className={styles.flexInnerColumn}
+            />
         ))}
-    </RadioGroup>
+    </div>
 );
 
 ThematicMapTypeSelect.propTypes = {
