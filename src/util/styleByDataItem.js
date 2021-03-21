47a0c96dc0458bc3386a468e747d2f77463def49
Bjørn Sandvik <bjorn@mastermaps.com>
2021-03-21 18:00:34 +0100

    fix: use name to identify style by data item

--- src/util/styleByDataItem.js
+++ src/util/styleByDataItem.js
@@ -163,17 +163,18 @@ export const styleByOptionSet = async config => {
     };
 
     // For easier and faster lookup below
-    const optionsByCode = optionSet.options.reduce((obj, option) => {
-        obj[option.code.toLowerCase()] = option;
+    const optionsByName = optionSet.options.reduce((obj, option) => {
+        obj[option.name.toLowerCase()] = option;
         return obj;
     }, {});
 
     // Add style data value and color to each feature
     config.data = config.data.map(feature => {
-        const code = feature.properties[id];
+        const name = feature.properties[id];
 
-        if (code) {
-            const option = optionsByCode[code.toLowerCase()];
+        if (name) {
+            const option = optionsByName[name.toLowerCase()];
+
             if (option) {
                 return {
                     ...feature,
