7e66a1131e13da210a0e4476684d8628369b46c4
Bjørn Sandvik <bjorn@mastermaps.com>
2021-06-07 20:35:45 +0200

    feat: period system settings

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -3,4 +3,9 @@ export const apiVersion = 36;
 export const SYSTEM_SETTINGS = [
     'keyAnalysisRelativePeriod',
     'keyBingMapsApiKey',
+    'keyHideDailyPeriods',
+    'keyHideWeeklyPeriods',
+    'keyHideBiWeeklyPeriods',
+    'keyHideMonthlyPeriods',
+    'keyHideBiMonthlyPeriods',
 ];
