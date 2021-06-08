edc1a67f5fe0678a451a83c08c1bf73389722582
Jen Jones Arnesen <jennifer@dhis2.org>
2021-06-08 16:53:16 +0200

    fix: diff threshold3

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,9 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand({
-    failureThreshold: 0.1,
+    failureThreshold: 0.5,
     // failureThresholdType: 'percent',
-    customDiffConfig: { threshold: 0.1 },
+    customDiffConfig: { threshold: 0.5 },
     // capture: 'viewport',
 });
 
