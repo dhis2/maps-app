9bc342ed99b02dc5f358ddb35f869303eaa8e20e
Bjørn Sandvik <bjorn@mastermaps.com>
2021-07-07 10:31:50 +0200

    chore: unit test

---
+++ src/components/orgunits/__tests__/OrgUnitProfile.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { OrgUnitProfile } from '../OrgUnitProfile';
+import Drawer from '../../core/Drawer';
+
+describe('Org unit profile (location details)', () => {
+    const renderWithProps = props => shallow(<OrgUnitProfile {...props} />);
+    let props;
+
+    beforeEach(() => {
+        props = {};
+    });
+
+    it('do not render if no org unit id is passed', () => {
+        expect(renderWithProps({ closeOrgUnit: jest.fn() }).type()).toBe(null);
+    });
+});
