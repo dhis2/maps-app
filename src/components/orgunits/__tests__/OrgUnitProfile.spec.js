44ac6c2a07464f25a8d6cb3ac3d2f4e1baecd0ac
Bjørn Sandvik <bjorn@mastermaps.com>
2021-07-07 14:14:31 +0200

    chore: org unit profile unit tests

--- src/components/orgunits/__tests__/OrgUnitProfile.spec.js
+++ src/components/orgunits/__tests__/OrgUnitProfile.spec.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { OrgUnitProfile } from '../OrgUnitProfile';
+import Drawer from '../../core/Drawer';
 
 describe('Org unit profile (location details)', () => {
     const renderWithProps = props => shallow(<OrgUnitProfile {...props} />);
-    let props;
-
-    beforeEach(() => {
-        props = {};
-    });
 
     it('do not render if no org unit id is passed', () => {
-        expect(renderWithProps({ closeOrgUnit: jest.fn() }).type()).toBe(null);
+        const wrapper = renderWithProps({ closeOrgUnit: jest.fn() });
+
+        expect(wrapper.type()).toBe(null);
+    });
+
+    it('should render a drawer if org unit id is passed', () => {
+        const wrapper = renderWithProps({ id: '123', closeOrgUnit: jest.fn() });
+
+        expect(wrapper.type()).toEqual(Drawer);
+    });
+
+    it('should call closeOrgUnit when drawer is closed', () => {
+        const closeOrgUnit = jest.fn();
+        const wrapper = renderWithProps({ id: '123', closeOrgUnit });
+
+        wrapper.find('.close').simulate('click');
+
+        expect(closeOrgUnit).toHaveBeenCalled();
     });
 });
