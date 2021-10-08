cec01b034600aa62f6e9cc54fc10b7b2a3384373
Jen Jones Arnesen <jennifer@dhis2.org>
2021-10-08 16:02:14 +0200

    feat: vector tile basemap

--- src/constants/basemaps.js
+++ src/constants/basemaps.js
@@ -2,13 +2,25 @@ import i18n from '@dhis2/d2-i18n';
 
 export const defaultBasemaps = () => [
     {
+        id: 'osmStreetView',
+        name: i18n.t('OSM Streetview'),
+        img: 'images/vector-basemap2.png',
+        config: {
+            type: 'vectorStyle',
+            url: '//basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
+            beforeId: 'watername_ocean',
+            attribution:
+                '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        },
+    },
+    {
         id: 'osmLight',
         name: i18n.t('OSM Light'),
         img: 'images/osmlight.png',
         config: {
-            type: 'tileLayer',
-            url:
-                '//cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+            type: 'vectorStyle',
+            url: '//basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+            beforeId: 'watername_ocean',
             attribution:
                 '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
         },
