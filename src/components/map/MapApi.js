247d48960712d7882b78c7b264969bbff5c3c253
Bjørn Sandvik <bjorn@mastermaps.com>
2021-12-01 14:52:50 +0100

    fix: include map fonts in app bundle

--- src/components/map/MapApi.js
+++ src/components/map/MapApi.js
@@ -8,16 +8,21 @@ import getMapLocale from './mapLocale';
 
 // Returns a new map instance
 const map = options => {
+    const baseUrl = process.env.DHIS2_BASE_URL;
+    const glyphs = `${baseUrl}/dhis-web-maps/fonts/{fontstack}/{range}.pbf`;
     const div = document.createElement('div');
 
     div.className = 'dhis2-map';
     div.style.width = '100%';
     div.style.height = '100%';
 
-    return new MapApi(div, {
+    const mapInstance = new MapApi(div, {
         ...options,
         locale: getMapLocale(),
+        glyphs,
     });
+
+    return mapInstance;
 };
 
 export { layerTypes, controlTypes, loadEarthEngineApi, poleOfInaccessibility };
