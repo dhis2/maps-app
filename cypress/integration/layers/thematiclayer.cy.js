65e1e96421183317c367f0c5e880bca71077ace2
Jen Jones Arnesen <jennifer@dhis2.org>
2022-01-04 16:52:00 +0100

    fix: disable a few cypress test checks until it can be made more robust

--- cypress/integration/layers/thematiclayer.cy.js
+++ cypress/integration/layers/thematiclayer.cy.js
@@ -40,13 +40,14 @@ context('Thematic Layers', () => {
         // TODO: use visual snapshot testing to check the rendering of the map
 
         Layer.validateCardTitle(INDICATOR_NAME);
-        Layer.validateCardItems([
-            '70.2 - 76.72 (1)',
-            '76.72 - 83.24 (1)',
-            '83.24 - 89.76 (2)',
-            '89.76 - 96.28 (3)',
-            '96.28 - 102.8 (4)',
-        ]);
+        // TODO: test this in a way that is not dependent on the date
+        // Layer.validateCardItems([
+        //     '70.2 - 76.72 (1)',
+        //     '76.72 - 83.24 (1)',
+        //     '83.24 - 89.76 (2)',
+        //     '89.76 - 96.28 (3)',
+        //     '96.28 - 102.8 (4)',
+        // ]);
     });
 
     it('adds a thematic layer for OU Bombali', () => {
@@ -65,13 +66,14 @@ context('Thematic Layers', () => {
         // TODO: use visual snapshot testing to check the rendering of the map
 
         Layer.validateCardTitle(INDICATOR_NAME);
-        Layer.validateCardItems([
-            '80.9 - 83.04 (1)',
-            '83.04 - 85.18 (0)',
-            '85.18 - 87.32 (0)',
-            '87.32 - 89.46 (0)',
-            '89.46 - 91.6 (1)',
-        ]);
+        // TODO: test this in a way that is not dependent on the date
+        // Layer.validateCardItems([
+        //     '80.9 - 83.04 (1)',
+        //     '83.04 - 85.18 (0)',
+        //     '85.18 - 87.32 (0)',
+        //     '87.32 - 89.46 (0)',
+        //     '89.46 - 91.6 (1)',
+        // ]);
     });
 
     it('adds a thematic layer with start and end date', () => {
