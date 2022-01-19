8b31b7b1188ab91f2d0bec6e4913aa1d4cd27aa8
Bjorn Sandvik <bjorn@dhis2.org>
2022-01-19 16:08:12 +0100

    feat: improved handling of earth engine layers (#2011)

--- src/components/map/MapLoadingMask.js
+++ src/components/map/MapLoadingMask.js
@@ -3,7 +3,7 @@ import { ComponentCover, CenteredContent, CircularLoader } from '@dhis2/ui';
 import styles from './styles/MapLoadingMask.module.css';
 
 const MapLoadingMask = () => (
-    <ComponentCover className={styles.cover}>
+    <ComponentCover translucent className={styles.cover}>
         <CenteredContent>
             <CircularLoader />
         </CenteredContent>
