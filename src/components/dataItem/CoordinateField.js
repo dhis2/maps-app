49d29aca42b8a7a62b4c29526308be9392a4c9ba
Bjørn Sandvik <bjorn@mastermaps.com>
2022-05-23 15:26:56 +0200

    feat: enrollment coordinate support

--- src/components/dataItem/CoordinateField.js
+++ src/components/dataItem/CoordinateField.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import i18n from '@dhis2/d2-i18n';
@@ -8,77 +8,34 @@ import {
     loadProgramStageDataElements,
 } from '../../actions/programs';
 
-export class CoordinateField extends Component {
-    static propTypes = {
-        value: PropTypes.string,
-        program: PropTypes.object,
-        programStage: PropTypes.object,
-        programAttributes: PropTypes.object.isRequired,
-        dataElements: PropTypes.object.isRequired,
-        loadProgramTrackedEntityAttributes: PropTypes.func.isRequired,
-        loadProgramStageDataElements: PropTypes.func.isRequired,
-        onChange: PropTypes.func.isRequired,
-        className: PropTypes.string,
-    };
-
-    componentDidMount() {
-        this.loadData();
-    }
-
-    componentDidUpdate(prevProps) {
-        const { program, onChange } = this.props;
-
-        if (program !== prevProps.program) {
-            onChange('event');
-        }
-
-        this.loadData();
-    }
-
-    render() {
-        const {
-            value,
-            program,
-            programStage,
-            programAttributes,
-            dataElements,
-            onChange,
-            className,
-        } = this.props;
-        let fields = [
+const CoordinateField = ({
+    value,
+    program,
+    programStage,
+    programAttributes,
+    dataElements,
+    loadProgramTrackedEntityAttributes,
+    loadProgramStageDataElements,
+    onChange,
+    className,
+}) => {
+    const fields = useMemo(() => {
+        const items = [
             { id: 'event', name: i18n.t('Event location') }, // Default coordinate field
+            { id: 'enrollment', name: i18n.t('Enrollment coordinate') },
         ];
 
-        if (program && programStage) {
-            fields = fields.concat(
-                [
-                    ...(programAttributes[program.id] || []),
-                    ...(dataElements[programStage.id] || []),
-                ].filter(field => field.valueType === 'COORDINATE')
-            );
-        }
+        return program && programStage
+            ? items.concat(
+                  [
+                      ...(programAttributes[program.id] || []),
+                      ...(dataElements[programStage.id] || []),
+                  ].filter(field => field.valueType === 'COORDINATE')
+              )
+            : items;
+    }, [program, programStage, programAttributes, dataElements]);
 
-        return (
-            <SelectField
-                label={i18n.t('Coordinate field')}
-                items={fields}
-                value={fields.find(f => f.id === value) ? value : 'event'}
-                onChange={field => onChange(field.id)}
-                className={className}
-            />
-        );
-    }
-
-    loadData() {
-        const {
-            program,
-            programStage,
-            programAttributes,
-            dataElements,
-            loadProgramTrackedEntityAttributes,
-            loadProgramStageDataElements,
-        } = this.props;
-
+    useEffect(() => {
         if (program && !programAttributes[program.id]) {
             loadProgramTrackedEntityAttributes(program.id);
         }
@@ -86,8 +43,41 @@ export class CoordinateField extends Component {
         if (programStage && !dataElements[programStage.id]) {
             loadProgramStageDataElements(programStage.id);
         }
-    }
-}
+    }, [
+        program,
+        programAttributes,
+        programStage,
+        dataElements,
+        loadProgramStageDataElements,
+        loadProgramStageDataElements,
+    ]);
+
+    useEffect(() => {
+        onChange('event');
+    }, [program, onChange]);
+
+    return (
+        <SelectField
+            label={i18n.t('Coordinate field')}
+            items={fields}
+            value={fields.find(f => f.id === value) ? value : 'event'}
+            onChange={field => onChange(field.id)}
+            className={className}
+        />
+    );
+};
+
+CoordinateField.propTypes = {
+    value: PropTypes.string,
+    program: PropTypes.object,
+    programStage: PropTypes.object,
+    programAttributes: PropTypes.object.isRequired,
+    dataElements: PropTypes.object.isRequired,
+    loadProgramTrackedEntityAttributes: PropTypes.func.isRequired,
+    loadProgramStageDataElements: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    className: PropTypes.string,
+};
 
 export default connect(
     state => ({
