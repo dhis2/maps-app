f5633eb12d287263f654c9986c4fc6969f5a1fb9
Bjorn Sandvik <bjorn@mastermaps.com>
2022-09-05 15:40:10 +0200

    fix: use tileScale to avoid ee aggregation timeout (DHIS2-13689, 2.38 backport) (#2283)

--- src/constants/earthEngine.js
+++ src/constants/earthEngine.js
@@ -207,6 +207,7 @@ export const earthEngineLayers = () => [
             palette: '#fee5d9,#fcbba1,#fc9272,#fb6a4a,#de2d26,#a50f15', // Reds
         },
         opacity: 0.9,
+        tileScale: 4,
     },
     {
         layer: EARTH_ENGINE_LAYER,
