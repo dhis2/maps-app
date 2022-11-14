fdcde4e331ee6ef1394e3e27701bb0e8a3b9d678
Jen Jones Arnesen <jennifer@dhis2.org>
2022-11-14 16:21:03 +0100

    fix: simplify, prevent recreating fns on render

--- src/components/map/layers/BasemapLayer.js
+++ src/components/map/layers/BasemapLayer.js
@@ -3,58 +3,54 @@ import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { setAlert } from '../../../actions/alerts';
 
+const removeLayer = async map => {
+    const layer = map.getLayerAtIndex(0);
+    if (layer) {
+        await map.removeLayer(layer);
+    }
+};
+
 const BasemapLayer = ({ id, config, opacity, isVisible, onError }, { map }) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        isVisible && id && addLayer();
-    }, [id, isVisible]);
+        const updateLayer = async () => {
+            await removeLayer(map);
+            if (isVisible && id) {
+                try {
+                    const layer = map.createLayer({
+                        ...config,
+                        id,
+                        index: 0,
+                        opacity,
+                        isVisible,
+                    });
+
+                    await map.addLayer(layer);
+                } catch (errorMessage) {
+                    const message = `Basemap could not be added: ${errorMessage}`;
+
+                    if (onError) {
+                        onError(message);
+                    } else {
+                        dispatch(
+                            setAlert({
+                                critical: true,
+                                message,
+                            })
+                        );
+                    }
+                }
+            }
+        };
+
+        updateLayer();
+    }, [id, isVisible, map, config]);
 
     useEffect(() => {
         const layer = map.getLayerAtIndex(0);
         layer?.setOpacity && layer.setOpacity(opacity);
-    }, [opacity]);
-
-    useEffect(() => {
-        if (!isVisible) {
-            removeLayer();
-        }
-    }, [isVisible]);
-
-    const addLayer = async () => {
-        await removeLayer();
-        try {
-            const theLayer = map.createLayer({
-                ...config,
-                id,
-                index: 0,
-                opacity,
-                isVisible,
-            });
-
-            await map.addLayer(theLayer);
-        } catch (errorMessage) {
-            const message = `Basemap could not be added: ${errorMessage}`;
-
-            if (onError) {
-                onError(message);
-            } else {
-                dispatch(
-                    setAlert({
-                        critical: true,
-                        message,
-                    })
-                );
-            }
-        }
-    };
-
-    const removeLayer = async () => {
-        const layer = map.getLayerAtIndex(0);
-        if (layer) {
-            await map.removeLayer(layer);
-        }
-    };
+    }, [opacity, map]);
 
     return null;
 };
