db8332125cfdaaba570c4d406e673df72ba8a38b
Bjørn Sandvik <bjorn@mastermaps.com>
2022-11-15 18:32:38 +0100

    chore: basemap layer refactor

--- src/components/map/layers/BasemapLayer.js
+++ src/components/map/layers/BasemapLayer.js
@@ -1,52 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import log from 'loglevel';
 
 const BASEMAP_LAYER_INDEX = 0;
 
 const BasemapLayer = ({ id, config, opacity, isVisible }, { map }) => {
-    const [layer, setLayer] = useState(null);
+    const basemap = useMemo(
+        () =>
+            map.createLayer({
+                ...config,
+                id,
+                index: BASEMAP_LAYER_INDEX,
+            }),
+        [id, config]
+    );
 
     useEffect(() => {
-        return function cleanup() {
-            if (layer) {
-                map.removeLayer(layer);
-            }
-        };
-    }, []);
+        try {
+            map.addLayer(basemap);
+        } catch (errorMessage) {
+            // TODO - use app-runtime alert system
+            log.error(`Basemap could not be added: ${errorMessage}`);
+        }
+        return () => map.removeLayer(basemap);
+    }, [map, basemap]);
 
     useEffect(() => {
-        const updateLayer = async () => {
-            if (layer) {
-                await map.removeLayer(layer);
-                setLayer(null);
-            }
-
-            if (isVisible && id) {
-                try {
-                    const newLayer = map.createLayer({
-                        ...config,
-                        id,
-                        index: BASEMAP_LAYER_INDEX,
-                        opacity,
-                        isVisible,
-                    });
-
-                    setLayer(newLayer); //even if addLayer throws, we need to keep a handle to the layer
-                    await map.addLayer(newLayer);
-                } catch (errorMessage) {
-                    // TODO - use app-runtime alert system
-                    log.error(`Basemap could not be added: ${errorMessage}`);
-                }
-            }
-        };
-
-        updateLayer();
-    }, [id, isVisible, map, config]); //layer should not be a dependency
+        basemap.setOpacity(opacity);
+    }, [basemap, opacity]);
 
     useEffect(() => {
-        layer?.setOpacity && layer.setOpacity(opacity);
-    }, [opacity, map]); //layer should not be a dependency
+        if (basemap.setVisibility) {
+            basemap.setVisibility(isVisible);
+        }
+    }, [map, basemap, isVisible]);
 
     return null;
 };
