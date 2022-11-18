6aaa525a691539c221281814c04d5e2d784e0701
Jen Jones Arnesen <jennifer@dhis2.org>
2022-11-18 09:57:06 +0100

    feat: add alert for failed basemap

--- src/components/map/layers/BasemapLayer.js
+++ src/components/map/layers/BasemapLayer.js
@@ -6,11 +6,21 @@ import i18n from '@dhis2/d2-i18n';
 
 const BASEMAP_LAYER_INDEX = 0;
 
+const getAlertError = message => {
+    return {
+        msg: i18n.t('Basemap could not be added: {{message}}', {
+            message,
+            nsSeparator: ';',
+        }),
+    };
+};
+
 const BasemapLayer = (
     { id, config, opacity, isVisible },
     { map, isPlugin }
 ) => {
-    const basemapErrorAlert = useAlert(({ msg }) => msg, { critical: true });
+    const basemapNotFoundAlert = useAlert(({ msg }) => msg, { critical: true });
+    const basemapInvalidAlert = useAlert(({ msg }) => msg, { critical: true });
     const basemap = useMemo(
         () =>
             map.createLayer({
@@ -25,12 +35,12 @@ const BasemapLayer = (
         map.addLayer(basemap).catch(errorMessage => {
             log.error(`Basemap could not be added: ${errorMessage}`);
             if (!isPlugin) {
-                basemapErrorAlert.show({
-                    msg: i18n.t('Basemap could not be added: {{message}}', {
-                        message: errorMessage,
-                        nsSeparator: ';',
-                    }),
-                });
+                const alertError = getAlertError(errorMessage);
+                if (errorMessage.includes('not found')) {
+                    basemapNotFoundAlert.show(alertError);
+                } else {
+                    basemapInvalidAlert.show(alertError);
+                }
             }
         });
         return () => map.removeLayer(basemap);
