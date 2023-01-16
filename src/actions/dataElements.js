eebcc80b4fd7d8f19ed5e334dc62675f45ad81f9
Bjorn Sandvik <bjorn@dhis2.org>
2023-01-16 15:05:38 +0100

    chore: data element select refactor (#2434)

--- src/actions/dataElements.js
+++ src/actions/dataElements.js
@@ -11,19 +11,6 @@ export const setDataElementGroups = (data) => ({
     payload: data,
 })
 
-// Load data elements in one group
-export const loadDataElements = (groupId) => ({
-    type: types.DATA_ELEMENTS_LOAD,
-    groupId,
-})
-
-// Set data elements for one group
-export const setDataElements = (groupId, payload) => ({
-    type: types.DATA_ELEMENTS_SET,
-    groupId,
-    payload,
-})
-
 // Load data element operands in one group
 export const loadDataElementOperands = (groupId) => ({
     type: types.DATA_ELEMENT_OPERANDS_LOAD,
