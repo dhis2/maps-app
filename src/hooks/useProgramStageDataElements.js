bbc0a01fbcd04f5ca6a63f03a6769908c9486cb4
Jen Jones Arnesen <jennifer@dhis2.org>
2023-01-24 16:10:21 +0100

    chore: useDataQuery for EventDataItemSelect, CoordinateField, FilterRow (#2450)
    * functional component
    * remove from redux and epics and use hooks with local state
    * Add LONG_TEXT as excluded type in EventDataItemSelect
    * shared code between CoordinateField, FilterRow/Group, StyleDataItem moved to a hook

---
+++ src/hooks/useProgramStageDataElements.js
@@ -0,0 +1,53 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+import { useState, useEffect } from 'react'
+import { useUserSettings } from '../components/UserSettingsProvider.js'
+import { getValidDataItems } from '../util/helpers.js'
+
+const PROGRAM_STAGE_DATA_ELEMENTS_QUERY = {
+    elements: {
+        resource: 'programStages',
+        id: ({ id }) => id,
+        params: ({ nameProperty }) => {
+            return {
+                fields: [
+                    `programStageDataElements[dataElement[id,${nameProperty}~rename(name),valueType,optionSet[id,displayName~rename(name)],legendSet]]`,
+                ],
+                paging: false,
+            }
+        },
+    },
+}
+export const useProgramStageDataElements = ({ programStageId }) => {
+    const [dataElements, setDataElements] = useState(null)
+    const { nameProperty } = useUserSettings()
+
+    const { refetch, loading } = useDataQuery(
+        PROGRAM_STAGE_DATA_ELEMENTS_QUERY,
+        {
+            lazy: true,
+            variables: { nameProperty },
+            onComplete: (data) => {
+                const elements = data.elements.programStageDataElements.map(
+                    (attr) => attr.dataElement
+                )
+
+                setDataElements(getValidDataItems(elements))
+            },
+        }
+    )
+
+    useEffect(() => {
+        setDataElements(null)
+
+        if (programStageId) {
+            refetch({
+                id: programStageId,
+            })
+        }
+    }, [programStageId, refetch])
+
+    return {
+        dataElements,
+        loading,
+    }
+}
