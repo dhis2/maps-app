6747d9a8d8dc823145c03d589462b0236040ae65
Bjørn Sandvik <bjorn@mastermaps.com>
2023-01-25 13:37:55 +0100

    chore: plugin refactor

--- src/components/plugin/Plugin.js
+++ src/components/plugin/Plugin.js
@@ -1,6 +1,7 @@
+import { useOnlineStatus } from '@dhis2/app-runtime'
 import { CssReset, CssVariables } from '@dhis2/ui'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { fetchLayer } from '../../loaders/layers.js'
 import { drillUpDown } from '../../util/map.js'
 import MapView from '../map/MapView.js'
@@ -14,96 +15,28 @@ const defaultBounds = [
     [50.2, 35.9],
 ]
 
-class Plugin extends Component {
-    static propTypes = {
-        basemap: PropTypes.object,
-        controls: PropTypes.array,
-        hideTitle: PropTypes.bool,
-        mapViews: PropTypes.array,
-        name: PropTypes.string,
+const Plugin = (props) => {
+    const { offline } = useOnlineStatus()
+    const [mapViews, setMapViews] = useState(props.mapViews)
+    const [contextMenuState, setContextMenuState] = useState({})
+    const [resizeCount] = useState(0)
+
+    const { name, basemap, hideTitle, controls } = props
+    const { position, offset, feature, isSplitView, container } =
+        contextMenuState
+
+    const onOpenContextMenu = (newState) => {
+        return setContextMenuState(newState)
     }
 
-    static defaultProps = {
-        hideTitle: false,
-    }
-
-    constructor(props, context) {
-        super(props, context)
-
-        this.state = {
-            isOffline: false,
-            mapViews: props.mapViews, // Can be changed by drilling
-            resizeCount: 0,
-        }
-    }
-
-    render() {
-        const { name, basemap, hideTitle, controls } = this.props
-        const {
-            position,
-            offset,
-            feature,
-            mapViews,
-            resizeCount,
-            isFullscreen,
-            isSplitView,
-            isOffline,
-            container,
-        } = this.state
-
-        return (
-            <div className={`dhis2-map-plugin ${styles.plugin}`}>
-                <CssReset />
-                <CssVariables colors spacers theme />
-                {!hideTitle && <MapName name={name} />}
-                <MapView
-                    isPlugin={true}
-                    isFullscreen={isFullscreen}
-                    basemap={basemap}
-                    layers={mapViews}
-                    controls={controls}
-                    bounds={defaultBounds}
-                    openContextMenu={this.onOpenContextMenu}
-                    resizeCount={resizeCount}
-                />
-                <Legend layers={mapViews} />
-                <ContextMenu
-                    feature={feature}
-                    position={position}
-                    offset={offset}
-                    onDrill={this.onDrill}
-                    onClose={this.onCloseContextMenu}
-                    isOffline={isOffline}
-                    isSplitView={isSplitView}
-                    container={container}
-                />
-            </div>
-        )
-    }
-
-    // Call this method when plugin container is resized
-    resize(isFullscreen) {
-        // Will trigger a redraw of the MapView component
-        this.setState((state) => ({
-            resizeCount: state.resizeCount + 1,
-            isFullscreen,
-        }))
-    }
-
-    setOfflineStatus(isOffline) {
-        this.setState({ isOffline })
-    }
-
-    onOpenContextMenu = (state) => this.setState(state)
-
-    onCloseContextMenu = () =>
-        this.setState({
+    const onCloseContextMenu = () =>
+        setContextMenuState({
             position: null,
             feature: null,
         })
 
-    onDrill = async (direction) => {
-        const { layerId, feature, mapViews } = this.state
+    const onDrill = async (direction) => {
+        const { layerId, feature } = contextMenuState
         let newConfig
 
         if (layerId && feature) {
@@ -134,15 +67,59 @@ class Plugin extends Component {
 
             const newLayer = await fetchLayer(newConfig)
 
-            this.setState({
-                mapViews: mapViews.map((layer) =>
+            setMapViews(
+                mapViews.map((layer) =>
                     layer.id === layerId ? newLayer : layer
-                ),
+                )
+            )
+
+            setContextMenuState({
                 position: null,
                 feature: null,
             })
         }
     }
+
+    return (
+        <div className={`dhis2-map-plugin ${styles.plugin}`}>
+            <CssReset />
+            <CssVariables colors spacers theme />
+            {!hideTitle && <MapName name={name} />}
+            <MapView
+                isPlugin={true}
+                isFullscreen={false}
+                basemap={basemap}
+                layers={mapViews}
+                controls={controls}
+                bounds={defaultBounds}
+                openContextMenu={onOpenContextMenu}
+                resizeCount={resizeCount}
+            />
+            <Legend layers={mapViews} />
+            <ContextMenu
+                feature={feature}
+                position={position}
+                offset={offset}
+                onDrill={onDrill}
+                onClose={onCloseContextMenu}
+                isOffline={offline}
+                isSplitView={isSplitView}
+                container={container}
+            />
+        </div>
+    )
+}
+
+Plugin.propTypes = {
+    basemap: PropTypes.object,
+    controls: PropTypes.array,
+    hideTitle: PropTypes.bool,
+    mapViews: PropTypes.array,
+    name: PropTypes.string,
+}
+
+Plugin.defaultProps = {
+    hideTitle: false,
 }
 
 export default Plugin
