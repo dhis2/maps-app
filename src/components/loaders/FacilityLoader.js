b5507dc5d20e0fb73a82654f69c269d5dfb8f2e9
Bjørn Sandvik <bjorn@mastermaps.com>
2023-01-26 09:36:26 +0100

    chore: revert facility loader

--- src/components/loaders/FacilityLoader.js
+++ src/components/loaders/FacilityLoader.js
@@ -1,96 +1,18 @@
-import { useDataQuery } from '@dhis2/app-runtime'
-import i18n from '@dhis2/d2-i18n'
 import PropTypes from 'prop-types'
 import { useEffect } from 'react'
-import { getOrgUnitsFromRows } from '../../util/analytics.js'
-import { toGeoJson } from '../../util/map.js'
-import { parseGroupSet, getStyledOrgUnits } from '../../util/orgUnits.js'
+import facilityLoader from '../../loaders/facilityLoader.js'
 
-const isPoint = (f) => f.geometry.type === 'Point'
-
-const facilityQuery = {
-    features: {
-        resource: 'geoFeatures',
-        params: ({
-            ou,
-            includeGroupSets = false,
-            displayProperty = 'NAME',
-        }) => ({
-            ou,
-            includeGroupSets,
-            displayProperty,
-        }),
-    },
-}
-
-const orgUnitGroupSetQuery = {
-    resource: 'organisationUnitGroupSets',
-    id: ({ id }) => id,
-    params: {
-        fields: 'organisationUnitGroups[id,name,color,symbol]',
-    },
-}
-
-const FacilityLoader = ({ config, onLoad }) => {
-    const { rows, organisationUnitGroupSet } = config
-
-    if (organisationUnitGroupSet) {
-        facilityQuery.groupSet = orgUnitGroupSetQuery
-    }
-
-    const { data, error, loading, refetch } = useDataQuery(facilityQuery, {
-        lazy: true,
-        // onComplete: console.log,
-    })
-
+const EventLoader = ({ config, onLoad }) => {
     useEffect(() => {
-        const orgUnits = getOrgUnitsFromRows(rows).map((item) => item.id)
-
-        refetch({
-            ou: `ou:${orgUnits.join(',')}`,
-            includeGroupSets: !!organisationUnitGroupSet,
-            id: organisationUnitGroupSet?.id,
-        })
-    }, [rows, organisationUnitGroupSet, refetch])
-
-    useEffect(() => {
-        if (data) {
-            const features = toGeoJson(data.features)
-            const groupSet = data.groupSet
-                ? parseGroupSet(data.groupSet)
-                : undefined
-            const contextPath = ''
-            const name = i18n.t('Facilities')
-
-            const { styledFeatures, legend } = getStyledOrgUnits(
-                features.filter(isPoint),
-                groupSet,
-                config,
-                contextPath
-            )
-
-            legend.title = name
-
-            onLoad({
-                ...config,
-                data: styledFeatures,
-                // associatedGeometries,
-                name,
-                legend,
-                // alerts,
-                isLoaded: true,
-                isExpanded: true,
-                isVisible: true,
-            })
-        }
-    }, [data, config, onLoad])
+        facilityLoader(config).then(onLoad)
+    }, [config, onLoad])
 
     return null
 }
 
-FacilityLoader.propTypes = {
+EventLoader.propTypes = {
     config: PropTypes.object.isRequired,
     onLoad: PropTypes.func.isRequired,
 }
 
-export default FacilityLoader
+export default EventLoader
