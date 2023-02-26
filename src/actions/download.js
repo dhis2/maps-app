967d5d4a75a8b8523130c0ddd349c15fca82a140
Bjørn Sandvik <bjorn@mastermaps.com>
2023-02-26 19:06:07 +0100

    chore: code cleaning

--- src/actions/download.js
+++ src/actions/download.js
@@ -1,12 +1,10 @@
 import * as types from '../constants/actionTypes.js'
 
-// Toggle download mode
 export const setDownloadMode = (payload) => ({
     type: types.DOWNLOAD_MODE_SET,
     payload,
 })
 
-// Set downlond property
 export const setDownloadConfig = (payload) => ({
     type: types.DOWNLOAD_CONFIG_SET,
     payload,
