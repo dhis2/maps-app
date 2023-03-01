17fd8d059f550d0a0d037cdf4f38ac8aae04fccd
Jen Jones Arnesen <jennifer@dhis2.org>
2023-03-01 09:11:20 +0100

    fix: this component is unused

--- src/components/plugin/MapContainer.js
+++ src/components/plugin/MapContainer.js
@@ -1,8 +1,9 @@
 import { useDataEngine } from '@dhis2/app-runtime'
 import PropTypes from 'prop-types'
 import React, { useState, useEffect, useMemo } from 'react'
-// import { getConfigFromNonMapConfig } from '../../util/getConfigFromNonMapConfig.js'
+import { getConfigFromNonMapConfig } from '../../util/getConfigFromNonMapConfig.js'
 import { getMigratedMapConfig } from '../../util/getMigratedMapConfig.js'
+import { mapFields } from '../../util/helpers.js'
 import LoadingMask from '../LoadingMask.js'
 import { useSystemSettings } from '../SystemSettingsProvider.js'
 import getBasemapConfig from './getBasemapConfig.js'
@@ -13,6 +14,7 @@ const MapContainer = ({ visualization }) => {
         basemap: basemapId,
         mapViews,
         userOrgUnit,
+        id,
         ...otherMapProps
     } = visualization
     const engine = useDataEngine()
@@ -20,8 +22,39 @@ const MapContainer = ({ visualization }) => {
     const [config, setConfig] = useState(null)
 
     const mapConfig = useMemo(() => {
+        if (id) {
+            //        mapRequest(config.id, keyDefaultBaseMap).then((favorite) =>
+            //             loadLayers({
+            //                 ...config,
+            //                 ...favorite,
+            //             })
+            //         )
+            // return d2.models.map
+            //     .get(id, {
+            //         fields: mapFields(),
+            //     })
+            //     .then((map) => getMigratedMapConfig(map, keyDefaultBaseMap))
+        } else if (!mapViews) {
+            return getConfigFromNonMapConfig(otherMapProps, keyDefaultBaseMap)
+        }
+
         return getMigratedMapConfig({ basemapId, mapViews }, keyDefaultBaseMap)
-    }, [basemapId, mapViews, keyDefaultBaseMap])
+    }, [id, otherMapProps, basemapId, mapViews, keyDefaultBaseMap])
+
+    // async function loadMap(config) {
+    //     if (config.id) {
+    //         mapRequest(config.id, keyDefaultBaseMap).then((favorite) =>
+    //             loadLayers({
+    //                 ...config,
+    //                 ...favorite,
+    //             })
+    //         )
+    //     } else if (!config.mapViews) {
+    //         getConfigFromNonMapConfig(config, keyDefaultBaseMap).then(
+    //             (config) => loadLayers(config)
+    //         )
+    //     }
+    // }
 
     useEffect(() => {
         const prepareConfig = async () => {
@@ -64,20 +97,3 @@ MapContainer.propTypes = {
 }
 
 export default MapContainer
-
-// async function loadMap(config) {
-// if (config.id) {
-//     mapRequest(config.id, keyDefaultBaseMap).then((favorite) =>
-//         loadLayers({
-//             ...config,
-//             ...favorite,
-//         })
-//     )
-// } else if (!config.mapViews) {
-//     getConfigFromNonMapConfig(config, keyDefaultBaseMap).then(
-//         (config) => loadLayers(config)
-//     )
-// } else {
-//     loadLayers(getMigratedMapConfig(config, keyDefaultBaseMap))
-// }
-// }
