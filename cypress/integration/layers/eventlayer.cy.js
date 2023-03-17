7fb47551235b4328df028579bbebb712f9e59e5c
Jen Jones Arnesen <jennifer@dhis2.org>
2023-03-17 15:51:24 +0100

    revert: reenable tests

--- cypress/integration/layers/eventlayer.cy.js
+++ cypress/integration/layers/eventlayer.cy.js
@@ -16,7 +16,7 @@ context('Event Layers', () => {
         cy.contains('Program is required').should('be.visible')
     })
 
-    it.skip('adds an event layer', () => {
+    it('adds an event layer', () => {
         Layer.openDialog('Events')
             .selectProgram('Inpatient morbidity and mortality')
             .validateStage('Inpatient morbidity and mortality')
