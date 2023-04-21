bb13f9288349e45d6ae85592eb0e67503588cf1b
Jen Jones Arnesen <jennifer@dhis2.org>
2023-04-21 12:22:41 +0200

    fix: need the downloads support for cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,42 @@
+const { existsSync, readdirSync, lstatSync } = require('fs')
+const path = require('path')
 const {
     networkShim,
     chromeAllowXSiteCookies,
 } = require('@dhis2/cypress-plugins')
 const { tagify } = require('cypress-tags')
+const fsExtra = require('fs-extra')
 const { getExcludedTags } = require('../support/getExcludedTags.js')
+
+const downloadsDirPath = 'cypress/downloads'
+
+const getLastDownloadFilePath = () => {
+    if (!existsSync(downloadsDirPath)) {
+        return null
+    }
+
+    const filesOrdered = readdirSync(downloadsDirPath)
+        .map((entry) => path.join(downloadsDirPath, entry))
+        .filter((entryWithPath) => lstatSync(entryWithPath).isFile())
+        .map((fileName) => ({ fileName, mtime: lstatSync(fileName).mtime }))
+        .sort((a, b) => b.mtime.getTime() - a.mtime.getTime())
+
+    if (!filesOrdered.length) {
+        return null
+    }
+
+    // TODO: this works only for chrome family browsers
+    if (filesOrdered[0].fileName.indexOf('crdownload') > -1) {
+        return null
+    }
+
+    return filesOrdered[0].fileName
+}
+
+const emptyDownloadsFolder = () => {
+    fsExtra.emptyDirSync(downloadsDirPath)
+    return null
+}
 
 module.exports = (on, config) => {
     networkShim(on)
@@ -23,4 +56,8 @@ module.exports = (on, config) => {
     config.env.CYPRESS_EXCLUDE_TAGS = excludedTags.join(',')
 
     on('file:preprocessor', tagify(config))
+    on('task', {
+        getLastDownloadFilePath,
+        emptyDownloadsFolder,
+    })
 }
