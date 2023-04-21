80824f1b243d57bfcbdfbfe2545427d0ccc0aeb8
Jen Jones Arnesen <jennifer@dhis2.org>
2023-04-21 11:21:05 +0200

    fix: add and update workflows and add cypress-tags

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,42 +1,26 @@
-const { existsSync, readdirSync, lstatSync } = require('fs')
-const path = require('path')
-const { chromeAllowXSiteCookies } = require('@dhis2/cypress-plugins')
-const fsExtra = require('fs-extra')
+const {
+    networkShim,
+    chromeAllowXSiteCookies,
+} = require('@dhis2/cypress-plugins')
+const { tagify } = require('cypress-tags')
+const { getExcludedTags } = require('../support/getExcludedTags.js')
 
-const downloadsDirPath = 'cypress/downloads'
-
-const getLastDownloadFilePath = () => {
-    if (!existsSync(downloadsDirPath)) {
-        return null
-    }
-
-    const filesOrdered = readdirSync(downloadsDirPath)
-        .map((entry) => path.join(downloadsDirPath, entry))
-        .filter((entryWithPath) => lstatSync(entryWithPath).isFile())
-        .map((fileName) => ({ fileName, mtime: lstatSync(fileName).mtime }))
-        .sort((a, b) => b.mtime.getTime() - a.mtime.getTime())
-
-    if (!filesOrdered.length) {
-        return null
-    }
-
-    // TODO: this works only for chrome family browsers
-    if (filesOrdered[0].fileName.indexOf('crdownload') > -1) {
-        return null
-    }
-
-    return filesOrdered[0].fileName
-}
-
-const emptyDownloadsFolder = () => {
-    fsExtra.emptyDirSync(downloadsDirPath)
-    return null
-}
-
-module.exports = (on) => {
+module.exports = (on, config) => {
+    networkShim(on)
     chromeAllowXSiteCookies(on)
-    on('task', {
-        getLastDownloadFilePath,
-        emptyDownloadsFolder,
-    })
+
+    if (!config.env.dhis2InstanceVersion) {
+        throw new Error(
+            'dhis2InstanceVersion is missing. Check the README for more information.'
+        )
+    }
+
+    const excludedTags = getExcludedTags(config.env.dhis2InstanceVersion)
+
+    console.log('instanceVersion', config.env.dhis2InstanceVersion)
+    console.log('tags to exclude from testing', excludedTags)
+
+    config.env.CYPRESS_EXCLUDE_TAGS = excludedTags.join(',')
+
+    on('file:preprocessor', tagify(config))
 }
