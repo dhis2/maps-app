6ec0eba15aca4b3f3b16563c53b6d88c24bc1b1e
Bjorn Sandvik <bjorn@dhis2.org>
2023-06-27 15:11:36 +0200

    fix: set period for each split view map (#2721)

--- src/components/map/MapPosition.js
+++ src/components/map/MapPosition.js
@@ -62,8 +62,11 @@ const MapPosition = () => {
     useEffect(() => {
         if (map) {
             const mapgl = map.getMapGL()
-            mapgl.setBearing(0)
-            mapgl.setPitch(0)
+
+            if (mapgl) {
+                mapgl.setBearing(0)
+                mapgl.setPitch(0)
+            }
         }
     }, [map, mapId])
 
@@ -79,7 +82,11 @@ const MapPosition = () => {
                 })
             })
 
-            setTimeout(() => map.resize(), 500)
+            setTimeout(() => {
+                if (map.getMapGL()) {
+                    map.resize()
+                }
+            }, 500)
         }
     }, [map, downloadMode])
 
