5fbf249fbe97e9d954cbde1e5e3f12665a0a066d
Bjorn Sandvik <bjorn@dhis2.org>
2023-08-09 11:09:19 +0200

    chore: BooleanStyle as functional component (#2921)

--- src/components/dataItem/BooleanStyle.js
+++ src/components/dataItem/BooleanStyle.js
@@ -1,7 +1,7 @@
 import i18n from '@dhis2/d2-i18n'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { setBooleanStyle } from '../../actions/layerEdit.js'
 import { qualitativeColors } from '../../constants/colors.js'
 import OptionStyle from '../optionSet/OptionStyle.js'
@@ -10,52 +10,49 @@ const style = {
     marginTop: 20,
 }
 
-class BooleanStyle extends Component {
-    static propTypes = {
-        setBooleanStyle: PropTypes.func.isRequired,
-        valueType: PropTypes.string.isRequired,
-        values: PropTypes.shape({
-            true: PropTypes.string.isRequired,
-            false: PropTypes.string,
-        }),
-    }
+const BooleanStyle = ({ valueType, values }) => {
+    const dispatch = useDispatch()
 
-    componentDidMount() {
-        const { valueType, values, setBooleanStyle } = this.props
-
+    useEffect(() => {
         if (!values) {
-            setBooleanStyle('true', qualitativeColors[0])
+            dispatch(setBooleanStyle('true', qualitativeColors[0]))
 
             if (valueType === 'BOOLEAN') {
-                setBooleanStyle('false', qualitativeColors[1])
+                dispatch(setBooleanStyle('false', qualitativeColors[1]))
             }
         }
+    }, [dispatch, valueType, values])
+
+    if (!values) {
+        return null
     }
 
-    render() {
-        const { valueType, values, setBooleanStyle } = this.props
-
-        if (!values) {
-            return null
-        }
-
-        return (
-            <div style={style}>
+    return (
+        <div style={style}>
+            <OptionStyle
+                name={i18n.t('Yes')}
+                color={values.true}
+                onChange={(color) => dispatch(setBooleanStyle('true', color))}
+            />
+            {valueType === 'BOOLEAN' && (
                 <OptionStyle
-                    name={i18n.t('Yes')}
-                    color={values.true}
-                    onChange={(color) => setBooleanStyle('true', color)}
+                    name={'No'}
+                    color={values.false}
+                    onChange={(color) =>
+                        dispatch(setBooleanStyle('false', color))
+                    }
                 />
-                {valueType === 'BOOLEAN' ? (
-                    <OptionStyle
-                        name={'No'}
-                        color={values.false}
-                        onChange={(color) => setBooleanStyle('false', color)}
-                    />
-                ) : null}
-            </div>
-        )
-    }
+            )}
+        </div>
+    )
 }
 
-export default connect(null, { setBooleanStyle })(BooleanStyle)
+BooleanStyle.propTypes = {
+    valueType: PropTypes.string.isRequired,
+    values: PropTypes.shape({
+        true: PropTypes.string.isRequired,
+        false: PropTypes.string,
+    }),
+}
+
+export default BooleanStyle
