2a9eed960c2df875054a9d353c5c44c86e15e249
Bjørn Sandvik <bjorn@mastermaps.com>
2023-08-14 15:06:38 +0200

    feat: load external layers from data store

--- src/actions/externalLayers.js
+++ src/actions/externalLayers.js
@@ -1,8 +1,11 @@
-import log from 'loglevel'
+// import log from 'loglevel'
 import * as types from '../constants/actionTypes.js'
-import { createExternalLayer } from '../util/external.js'
-import { fetchExternalLayers } from '../util/requests.js'
-import { addBasemaps } from './basemap.js'
+// import { createExternalLayer } from '../util/external.js'
+// import { fetchExternalLayers } from '../util/requests.js'
+// import { addBasemaps } from './basemap.js'
+import { EARTH_ENGINE_LAYER } from '../constants/layers.js'
+
+export const EXTERNAL_LAYERS_NAMESPACE = 'EXTERNAL_LAYERS'
 
 const isBasemap = (layer) => layer.mapLayerPosition === 'BASEMAP'
 const isOverlay = (layer) => !isBasemap(layer)
@@ -15,6 +18,45 @@ export const addExternalLayer = (layer) => ({
 
 export const tSetExternalLayers = (engine) => async (dispatch) => {
     console.log('engine', engine)
+
+    engine
+        .query({
+            dataStore: {
+                resource: 'dataStore',
+            },
+        })
+        .then(({ dataStore }) => {
+            if (dataStore.includes(EXTERNAL_LAYERS_NAMESPACE)) {
+                engine
+                    .query({
+                        layerIds: {
+                            resource: `dataStore/${EXTERNAL_LAYERS_NAMESPACE}`,
+                        },
+                    })
+                    .then(({ layerIds }) => {
+                        Promise.all(
+                            layerIds.map((layerId) =>
+                                engine
+                                    .query({
+                                        layer: {
+                                            resource: `dataStore/${EXTERNAL_LAYERS_NAMESPACE}/${layerId}`,
+                                        },
+                                    })
+                                    .then(({ layer }) => layer)
+                            )
+                        ).then((layers) => {
+                            layers.map((layer) =>
+                                dispatch(
+                                    addExternalLayer({
+                                        ...layer,
+                                        layer: EARTH_ENGINE_LAYER, // TODO
+                                    })
+                                )
+                            )
+                        })
+                    })
+            }
+        })
 
     /*
     try {
