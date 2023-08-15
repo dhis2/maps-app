730c0d7ce8a1c6eb3ff4e71a76714405c3fce5b3
Jen Jones Arnesen <jennifer@dhis2.org>
2023-08-15 18:24:15 +0200

    fix: remove css grid styling not in use

--- src/components/app/AppMenu.js
+++ src/components/app/AppMenu.js
@@ -5,19 +5,16 @@ import DownloadButton from '../download/DownloadButton.js'
 import InterpretationsToggle from '../interpretations/InterpretationsToggle.js'
 import AddLayerButton from '../layers/overlays/AddLayerButton.js'
 import FileMenu from './FileMenu.js'
-import styles from './styles/AppMenu.module.css'
 
 const AppMenu = ({ onFileMenuAction }) => (
-    <div className={styles.toolbar}>
-        <Toolbar>
-            <AddLayerButton />
-            <HoverMenuBar>
-                <FileMenu onFileMenuAction={onFileMenuAction} />
-                <DownloadButton />
-            </HoverMenuBar>
-            <InterpretationsToggle />
-        </Toolbar>
-    </div>
+    <Toolbar>
+        <AddLayerButton />
+        <HoverMenuBar>
+            <FileMenu onFileMenuAction={onFileMenuAction} />
+            <DownloadButton />
+        </HoverMenuBar>
+        <InterpretationsToggle />
+    </Toolbar>
 )
 
 AppMenu.propTypes = {
