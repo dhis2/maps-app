f532a81463e1842a3e3aa938497505d290be028a
Jen Jones Arnesen <jennifer@dhis2.org>
2023-09-25 16:25:09 +0200

    revert: "chore: fixed period select refactor (#2958)" (#2984)
    This reverts commit 8fdd67eb0b57e3c185cda9da3486d727dc2a64c7.

--- src/components/edit/thematic/ValueTypeSelect.js
+++ src/components/edit/thematic/ValueTypeSelect.js
@@ -1,35 +1,32 @@
 import i18n from '@dhis2/d2-i18n'
 import PropTypes from 'prop-types'
-import React, { useMemo } from 'react'
+import React from 'react'
 import { dimConf } from '../../../constants/dimension.js'
 import { SelectField } from '../../core/index.js'
 
-const getValueTypes = () => [
-    { id: dimConf.indicator.objectName, name: i18n.t('Indicator') },
-    { id: dimConf.dataElement.objectName, name: i18n.t('Data element') },
-    { id: dimConf.dataSet.objectName, name: i18n.t('Reporting rates') },
-    {
-        id: dimConf.eventDataItem.objectName,
-        name: i18n.t('Event data items'),
-    },
-    {
-        id: dimConf.programIndicator.objectName,
-        name: i18n.t('Program indicators'),
-    },
-    {
-        id: dimConf.calculation.objectName,
-        name: i18n.t('Calculations'),
-    },
-]
-
-const ValueTypeSelect = ({ value, onChange, className }) => {
-    const items = useMemo(() => getValueTypes(), [])
+const ValueTypeSelect = (props) => {
+    const { value, onChange, className } = props
 
     // If value type is data element operand, make it data element
     const type =
         value === dimConf.operand.objectName
             ? dimConf.dataElement.objectName
             : value
+
+    // TODO: Avoid creating on each render (needs to be created after i18next contains translations
+    const items = [
+        { id: dimConf.indicator.objectName, name: i18n.t('Indicator') },
+        { id: dimConf.dataElement.objectName, name: i18n.t('Data element') },
+        { id: dimConf.dataSet.objectName, name: i18n.t('Reporting rates') },
+        {
+            id: dimConf.eventDataItem.objectName,
+            name: i18n.t('Event data items'),
+        },
+        {
+            id: dimConf.programIndicator.objectName,
+            name: i18n.t('Program indicators'),
+        },
+    ]
 
     return (
         <SelectField
@@ -38,7 +35,6 @@ const ValueTypeSelect = ({ value, onChange, className }) => {
             value={type}
             onChange={(valueType) => onChange(valueType.id)}
             className={className}
-            dataTest="thematic-layer-value-type-select"
         />
     )
 }
