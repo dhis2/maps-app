e77d7fdd1df79c4fbf0e24bb5c30cbd2defc1626
Jen Jones Arnesen <jennifer@dhis2.org>
2023-10-19 10:21:09 +0200

    chore: set bgcolor and font color for color cell

--- src/components/datatable/UiDataTable.js
+++ src/components/datatable/UiDataTable.js
@@ -19,12 +19,19 @@ import React, {
     useMemo,
 } from 'react'
 import { useSelector } from 'react-redux'
+import {
+    EVENT_LAYER,
+    THEMATIC_LAYER,
+    ORG_UNIT_LAYER,
+    // EARTH_ENGINE_LAYER,
+} from '../../constants/layers.js'
+import { isDarkColor } from '../../util/colors.js'
 import styles from './styles/UiDataTable.module.css'
 
 const ASCENDING = 'asc'
 const DESCENDING = 'desc'
 
-const thematicHeaders = [
+const getThematicHeaders = () => [
     { name: i18n.t('Index'), dataKey: 'index' },
     { name: i18n.t('Name'), dataKey: 'name' },
     { name: i18n.t('Id'), dataKey: 'id' },
@@ -35,15 +42,52 @@ const thematicHeaders = [
     { name: i18n.t('Parent'), dataKey: 'parentName' },
     { name: i18n.t('Type'), dataKey: 'type' },
     { name: i18n.t('Color'), dataKey: 'color' },
-    { name: i18n.t('Name2'), dataKey: 'name2' },
-    { name: i18n.t('Id2'), dataKey: 'id2' },
-    { name: i18n.t('Value2'), dataKey: 'value2' },
-    { name: i18n.t('Legend2'), dataKey: 'legend2' },
-    { name: i18n.t('Range2'), dataKey: 'range2' },
-    { name: i18n.t('Level2'), dataKey: 'level2' },
-    { name: i18n.t('Parent2'), dataKey: 'parentName2' },
-    { name: i18n.t('Type2'), dataKey: 'type2' },
-    { name: i18n.t('Color2'), dataKey: 'color2' },
+    // { name: i18n.t('Name2'), dataKey: 'name2' },
+    // { name: i18n.t('Id2'), dataKey: 'id2' },
+    // { name: i18n.t('Value2'), dataKey: 'value2' },
+    // { name: i18n.t('Legend2'), dataKey: 'legend2' },
+    // { name: i18n.t('Range2'), dataKey: 'range2' },
+    // { name: i18n.t('Level2'), dataKey: 'level2' },
+    // { name: i18n.t('Parent2'), dataKey: 'parentName2' },
+    // { name: i18n.t('Type2'), dataKey: 'type2' },
+    // { name: i18n.t('Color2'), dataKey: 'color2' },
+]
+
+const thematicFields = [
+    'index',
+    'name',
+    'id',
+    'value',
+    'legend',
+    'range',
+    'level',
+    'parentName',
+    'type',
+    'color',
+]
+
+const getEventHeaders = (layer) => {
+    return [
+        { name: i18n.t('Index'), dataKey: 'index' },
+        { name: i18n.t('Org unit'), dataKey: 'ouname' },
+        { name: i18n.t('Id'), dataKey: 'id' },
+        { name: i18n.t('Event time'), dataKey: 'eventdate' },
+        { name: i18n.t('Type'), dataKey: 'type' },
+        // { name: i18n.t('Color'), dataKey: 'color' },
+    ]
+}
+
+const getOrgUnitHeaders = () => [
+    { name: i18n.t('Index'), dataKey: 'index' },
+    { name: i18n.t('Name'), dataKey: 'name' },
+    { name: i18n.t('Id'), dataKey: 'id' },
+    { name: i18n.t('Value'), dataKey: 'value' },
+    { name: i18n.t('Legend'), dataKey: 'legend' },
+    { name: i18n.t('Range'), dataKey: 'range' },
+    { name: i18n.t('Level'), dataKey: 'level' },
+    { name: i18n.t('Parent'), dataKey: 'parentName' },
+    { name: i18n.t('Type'), dataKey: 'type' },
+    { name: i18n.t('Color'), dataKey: 'color' },
 ]
 
 const Table = () => {
@@ -61,6 +105,18 @@ const Table = () => {
 
     const layer = mapViews.find((l) => l.id === activeLayerId)
     const { data } = layer
+
+    const getHeaders = () => {
+        if (layer.layer === THEMATIC_LAYER) {
+            return getThematicHeaders()
+        } else if (layer.layer === EVENT_LAYER) {
+            return getEventHeaders(layer)
+        } else if (layer.layer === ORG_UNIT_LAYER) {
+            return getOrgUnitHeaders(layer)
+        }
+        //  else if (layer.layer === EARTH_ENGINE_LAYER) {
+        // }
+    }
 
     const rawData = useMemo(
         () =>
@@ -95,27 +151,12 @@ const Table = () => {
         })
 
         setRows(
-            rawData.map((row) => [
-                row.index,
-                row.name,
-                row.id,
-                row.value,
-                row.legend,
-                row.range,
-                row.level,
-                row.parentName,
-                row.type,
-                row.color,
-                row.name,
-                row.id,
-                row.value,
-                row.legend,
-                row.range,
-                row.level,
-                row.parentName,
-                row.type,
-                row.color,
-            ])
+            rawData.map((item) => {
+                return thematicFields.map((key) => ({
+                    value: item[key],
+                    dataKey: key,
+                }))
+            })
         )
     }, [rawData, sortField, sortDirection])
 
@@ -136,7 +177,7 @@ const Table = () => {
         <DataTable scrollHeight={'100%'} scrollWidth={'100%'} width={'100%'}>
             <DataTableHead>
                 <DataTableRow>
-                    {thematicHeaders.map(({ name, dataKey }, index) => (
+                    {getHeaders().map(({ name, dataKey }, index) => (
                         <DataTableColumnHeader
                             fixed
                             top="0"
@@ -161,14 +202,21 @@ const Table = () => {
             <DataTableBody>
                 {rows.map((row, index) => (
                     <DataTableRow key={`dtrow-${index}`}>
-                        {row.map((value, cellindex) => (
+                        {row.map(({ dataKey, value }) => (
                             <DataTableCell
-                                key={`dtcell-${cellindex}`}
+                                key={`dtcell-${dataKey}`}
                                 className={cx(
-                                    styles.cell,
                                     styles.fontClass,
-                                    styles.sizeClass
+                                    styles.sizeClass,
+                                    {
+                                        [styles.darkText]:
+                                            dataKey === 'color' &&
+                                            isDarkColor(value),
+                                    }
                                 )}
+                                backgroundColor={
+                                    dataKey === 'color' ? value : null
+                                }
                             >
                                 {value}
                             </DataTableCell>
