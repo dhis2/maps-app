a3989b08bd723373c07149d00e18321394af4987
Jen Jones Arnesen <jennifer@dhis2.org>
2023-11-01 08:34:54 +0100

    chore: set up report portal

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,35 @@ module.exports = {
     },
     testURL: 'http://localhost/',
     snapshotSerializers: ['enzyme-to-json/serializer'],
+
+    testRunner: 'jest-circus/runner',
+    testRegex: ['/src/modules/__tests__/.*.spec.js?$'],
+    reporters: [
+        'default',
+        [
+            '@reportportal/agent-js-jest',
+            {
+                apiKey: process.env.REPORTPORTAL_API_KEY,
+                endpoint: process.env.REPORTPORTAL_ENDPOINT,
+                project: process.env.REPORTPORTAL_PROJECT,
+                launch: 'maps_app_master',
+                attributes: [
+                    {
+                        key: 'version',
+                        value: 'master',
+                    },
+                    {
+                        key: 'app_name',
+                        value: 'maps_app',
+                    },
+                    {
+                        key: 'test_level',
+                        value: 'unit/integration',
+                    },
+                ],
+                description: '',
+                debug: true,
+            },
+        ],
+    ],
 }
