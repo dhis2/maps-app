e006287f55ea460b03e8a2636059e27ce3bfd8fc
Jen Jones Arnesen <jennifer@dhis2.org>
2023-11-01 15:43:59 +0100

    fix: sorting of layers by drag and drop was not working [DHIS2-16024] (#3021)
    Fixes https://dhis2.atlassian.net/browse/DHIS2-16024

---
+++ cypress/integration/layers/multilayers.cy.js
@@ -0,0 +1,59 @@
+import { getMaps } from '../../elements/map_canvas.js'
+import { OrgUnitLayer } from '../../elements/orgunit_layer.js'
+import { ThematicLayer } from '../../elements/thematic_layer.js'
+
+const INDICATOR_NAME = 'ANC 1 Coverage'
+
+describe('Multiple Layers', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    const TLayer = new ThematicLayer()
+    const OULayer = new OrgUnitLayer()
+
+    it('adds a thematic layer and an orgunit layer', () => {
+        TLayer.openDialog('Thematic')
+            .selectIndicatorGroup('ANC')
+            .selectIndicator(INDICATOR_NAME)
+            .selectTab('Period')
+            .selectPeriodType('Yearly')
+            .selectTab('Org Units')
+            .selectOu('Sierra Leone')
+            .selectOuLevel('District')
+            .addToMap()
+
+        TLayer.validateDialogClosed(true)
+
+        TLayer.validateCardTitle(INDICATOR_NAME)
+
+        getMaps().should('have.length', 1)
+
+        OULayer.openDialog('Org units')
+            .selectOu('Sierra Leone')
+            .selectOuLevel('District')
+            .addToMap()
+
+        OULayer.validateDialogClosed(true)
+
+        OULayer.validateCardTitle('Organisation units')
+        OULayer.validateCardItems(['District'])
+
+        cy.getByDataTest('sortable-layers-list')
+            .children()
+            .should('have.length', 2)
+
+        // confirm the order of the cards
+        cy.getByDataTest('sortable-layers-list')
+            .children()
+            .first()
+            .should('contain', 'Organisation units')
+
+        cy.getByDataTest('sortable-layers-list')
+            .children()
+            .last()
+            .should('contain', INDICATOR_NAME)
+
+        // TODO - add a test for drag/drop reordering of the layers
+    })
+})
