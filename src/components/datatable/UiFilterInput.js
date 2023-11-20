79912a2213afe2700339e8bdb19a0a83081cb90f
Jen Jones Arnesen <jennifer@dhis2.org>
2023-11-20 12:08:40 -0800

    chore: dataTable ee and facility layers

---
+++ src/components/datatable/UiFilterInput.js
@@ -0,0 +1,56 @@
+import i18n from '@dhis2/d2-i18n'
+import { Input } from '@dhis2/ui'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setDataFilter, clearDataFilter } from '../../actions/dataFilters.js'
+import styles from './styles/UiFilterInput.module.css'
+
+// http://adazzle.github.io/react-data-grid/examples.html#/custom-filters
+// https://github.com/adazzle/react-data-grid/tree/master/packages/react-data-grid-addons/src/cells/headerCells/filters
+const FilterInput = ({ type, dataKey }) => {
+    const dispatch = useDispatch()
+    const dataTable = useSelector((state) => state.dataTable)
+    const map = useSelector((state) => state.map)
+
+    const overlay =
+        dataTable && map.mapViews.filter((layer) => layer.id === dataTable)[0]
+
+    let layerId
+    let filters
+    if (overlay) {
+        layerId = overlay.id
+        filters = overlay.dataFilters || {}
+    }
+
+    const filterValue = filters[dataKey] || ''
+
+    // https://stackoverflow.com/questions/36683770/react-how-to-get-the-value-of-an-input-field
+    const onChange = (evt) => {
+        const value = evt.target.value
+
+        if (value !== '') {
+            dispatch(setDataFilter(layerId, dataKey, value))
+        } else {
+            dispatch(clearDataFilter(layerId, dataKey, value))
+        }
+    }
+
+    // TODO: Support more field types
+    return (
+        <input
+            className={styles.filterInput}
+            placeholder={type === 'number' ? '2,>3&<8' : i18n.t('Filter')}
+            value={filterValue}
+            onClick={(evt) => evt.stopPropagation()}
+            onChange={onChange}
+        />
+    )
+}
+
+FilterInput.propTypes = {
+    dataKey: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+}
+
+export default FilterInput
---
+++ src/util/dataTable.js
@@ -0,0 +1,177 @@
+import i18n from '@dhis2/d2-i18n'
+import { isValidUid } from 'd2/uid' // TODO replace
+import {
+    EVENT_LAYER,
+    THEMATIC_LAYER,
+    ORG_UNIT_LAYER,
+    GEOJSON_URL_LAYER,
+    EXTERNAL_LAYER,
+    FACILITY_LAYER,
+    EARTH_ENGINE_LAYER,
+} from '../constants/layers.js'
+import { numberValueTypes } from '../constants/valueTypes.js'
+import { hasClasses, getPrecision } from './earthEngine.js'
+import { numberPrecision } from './numbers.js'
+// import { get } from 'lodash'
+
+const TYPE_NUMBER = 'number'
+const TYPE_STRING = 'string'
+const TYPE_DATE = 'date'
+
+const getIndexHeader = () => ({
+    name: i18n.t('Index'),
+    dataKey: 'index',
+    // type: TYPE_STRING,
+})
+
+const getIdHeader = () => ({
+    name: i18n.t('Id'),
+    dataKey: 'id',
+    type: TYPE_STRING,
+})
+
+const getNameHeader = (isOu = false) => ({
+    name: isOu ? i18n.t('Org unit') : i18n.t('Name'),
+    dataKey: isOu ? 'ouname' : 'name',
+    type: TYPE_STRING,
+})
+
+const getTypeHeader = () => ({
+    name: i18n.t('Type'),
+    dataKey: 'type',
+    type: TYPE_STRING,
+})
+
+const getThematicHeaders = () => [
+    getIndexHeader(),
+    getNameHeader(),
+    getIdHeader(),
+    { name: i18n.t('Value'), dataKey: 'value', type: TYPE_NUMBER },
+    { name: i18n.t('Legend'), dataKey: 'legend', type: TYPE_STRING },
+    { name: i18n.t('Range'), dataKey: 'range', type: TYPE_STRING },
+    { name: i18n.t('Level'), dataKey: 'level', type: TYPE_NUMBER },
+    { name: i18n.t('Parent'), dataKey: 'parentName', type: TYPE_STRING },
+    getTypeHeader(),
+]
+
+const getEventHeaders = (layer) => {
+    const defaultFieldsStart = [
+        getIndexHeader(),
+        getNameHeader(true),
+        getIdHeader,
+        {
+            name: i18n.t('Event time'),
+            dataKey: 'eventdate',
+            type: TYPE_DATE,
+            renderer: 'formatTime...',
+        },
+    ]
+
+    const { headers = [] } = layer
+
+    const customFields = headers
+        .filter(({ name }) => isValidUid(name))
+        .map(({ name, column, valueType }) => ({
+            name: column,
+            dataKey: name,
+            type: numberValueTypes.includes(valueType)
+                ? TYPE_NUMBER
+                : TYPE_STRING,
+        }))
+
+    const defaultFieldsEnd = [{ name: i18n.t('Type'), dataKey: 'type' }]
+
+    return defaultFieldsStart.concat(customFields).concat(defaultFieldsEnd)
+}
+
+const getOrgUnitHeaders = () => [
+    getIndexHeader(),
+    getNameHeader(),
+    getIdHeader(),
+    { name: i18n.t('Value'), dataKey: 'value' },
+    { name: i18n.t('Legend'), dataKey: 'legend' },
+    { name: i18n.t('Range'), dataKey: 'range' },
+    { name: i18n.t('Level'), dataKey: 'level' },
+    { name: i18n.t('Parent'), dataKey: 'parentName' },
+    getTypeHeader(),
+    // { name: i18n.t('Color'), dataKey: 'color' },
+]
+
+const getFacilityHeaders = () => [
+    getIndexHeader(),
+    getNameHeader(),
+    getIdHeader(),
+    getTypeHeader(),
+]
+
+const getGeoJsonUrlHeaders = (layer) => {
+    const row = layer.data[0]
+    const headers = Object.entries(row.properties).map(([key, value]) => ({
+        name: key,
+        dataKey: key,
+        type: typeof value === 'number' ? TYPE_NUMBER : TYPE_STRING,
+    }))
+    return headers
+}
+
+const getEarthEngineHeaders = ({ aggregationType, legend }) => {
+    const { title, items } = legend
+
+    let eeheaders = []
+
+    if (hasClasses(aggregationType) && items) {
+        eeheaders = items.map(({ id, name }) => ({
+            name,
+            dataKey: String(id),
+            type: TYPE_NUMBER,
+        }))
+    } else if (Array.isArray(aggregationType) && aggregationType.length) {
+        eeheaders = aggregationType.map((type) => ({
+            name: `${type} ${title}`,
+            dataKey: type,
+            type: TYPE_NUMBER,
+        }))
+    }
+
+    return [
+        getIndexHeader(),
+        getNameHeader(),
+        getIdHeader(),
+        getTypeHeader(),
+    ].concat(eeheaders)
+}
+
+export const getBasicHeaders = (layer) => {
+    switch (layer.layer) {
+        case THEMATIC_LAYER:
+            return getThematicHeaders()
+        case EVENT_LAYER:
+            return getEventHeaders(layer)
+        case ORG_UNIT_LAYER:
+            return getOrgUnitHeaders(layer)
+        case GEOJSON_URL_LAYER:
+        case EXTERNAL_LAYER:
+            return getGeoJsonUrlHeaders(layer)
+        case FACILITY_LAYER:
+            return getFacilityHeaders(layer)
+        case EARTH_ENGINE_LAYER:
+            return getEarthEngineHeaders(layer)
+        default:
+            // Handle the default case if needed
+            break
+    }
+}
+
+export const getHeaders = (layer, styleDataItem) => {
+    const headers = getBasicHeaders(layer)
+
+    if (layer.layer === THEMATIC_LAYER || styleDataItem) {
+        headers.push({
+            name: i18n.t('Color'),
+            dataKey: 'color',
+            renderer: 'rendercolor',
+        })
+    }
+
+    return headers
+}
