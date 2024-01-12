b25267d33a8ab5a136f34516ba1bc802ae9e51b3
Jen Jones Arnesen <jennifer@dhis2.org>
2024-01-12 08:30:10 +0100

    feat: add support for hash routing in line with other analytics apps [DHIS2-15762] (#3009)
    Implements https://dhis2.atlassian.net/browse/DHIS2-15762
    
    Continue to support the old url patterns for backwards compatibility
    
    Supports:
    
    /#/ZBjCfSaLSqD
    /#/ZBjCfSaLSqD/download
    /#/ZBjCfSaLSqD?interpretationId=yKqhXZdeJ6a
    /#/ZBjCfSaLSqD?interpretationId=yKqhXZdeJ6a&initialFocus=true
    /#/currentAnalyticalObject
    /#/download (new map, in download mode)
    
    // legacy urls
    /?id=ZBjCfSaLSqD
    /?id=ZBjCfSaLSqD&interpretationid=yKqhXZdeJ6a
    /?id=ZBjCfSaLSqD&interpretationId=yKqhXZdeJ6a
    /?currentAnalyticalObject=true
    
    The app supports legacy urls (see above) because dashboard-app
    still produces the legacy url for both opening the map, and opening an interpretation.
    In addition, urls to maps may have been shared in other ways.
    In maps-app, legacy urls are replaced with the new url style when the
    maps-app first opens (AppWrapper)
    
    FileMenu: push paths to history for the various actions.
    Also, some renaming was done in the file to (hopefully) increase clarity.
    
    useLoadMap: this is where all the map loading and history listening is set up.
    
    Refactoring. Previously there were 3 components that dealt with initiating the maps app:
    AppWrapper, App, AppLayout. App has become the former "AppLayout",
    and all of the loading and navigating logic has been moved to the new custom hook useLoadMap

--- src/components/download/DownloadButton.js
+++ src/components/download/DownloadButton.js
@@ -1,17 +1,11 @@
 import i18n from '@dhis2/d2-i18n'
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { setDownloadMode } from '../../actions/download.js'
+import { openDownloadMode } from '../../util/history.js'
 import styles from './styles/DownloadButton.module.css'
 
 const DownloadButton = () => {
-    const dispatch = useDispatch()
-
     return (
-        <button
-            className={styles.button}
-            onClick={() => dispatch(setDownloadMode(true))}
-        >
+        <button className={styles.button} onClick={openDownloadMode}>
             {i18n.t('Download')}
         </button>
     )
