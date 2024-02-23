1b52fde69d9d5d8fbb92c3e1ce2ce18fe9420e7a
Jen Jones Arnesen <jennifer@dhis2.org>
2024-02-23 16:00:45 +0100

    fix:keep redux out of loaders since they are used by plugin

--- src/components/loaders/EventLoader.js
+++ src/components/loaders/EventLoader.js
@@ -1,10 +1,8 @@
 import PropTypes from 'prop-types'
 import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
 import eventLoader from '../../loaders/eventLoader.js'
 
-const EventLoader = ({ config, onLoad }) => {
-    const dataTableOpen = useSelector((state) => !!state.dataTable)
+const EventLoader = ({ config, dataTableOpen, onLoad }) => {
     useEffect(() => {
         eventLoader(config, dataTableOpen).then(onLoad)
     }, [config, onLoad, dataTableOpen])
@@ -14,6 +12,7 @@ const EventLoader = ({ config, onLoad }) => {
 
 EventLoader.propTypes = {
     config: PropTypes.object.isRequired,
+    dataTableOpen: PropTypes.bool.isRequired,
     onLoad: PropTypes.func.isRequired,
 }
 
