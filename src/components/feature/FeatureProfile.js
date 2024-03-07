a3bddc477263fbd5bcd940cedf6b8a443a0091ce
Jen Jones Arnesen <jennifer@dhis2.org>
2024-03-07 15:25:14 +0100

    chore: use DrawerContent component

--- src/components/feature/FeatureProfile.js
+++ src/components/feature/FeatureProfile.js
@@ -1,43 +1,48 @@
 import i18n from '@dhis2/d2-i18n'
 import { IconCross24 } from '@dhis2/ui'
+import PropTypes from 'prop-types'
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { closeFeatureProfile } from '../../actions/feature.js'
 import Drawer from '../core/Drawer.js'
 import styles from './styles/FeatureProfile.module.css'
 
+const DrawerContent = ({ data }) => {
+    if (Object.keys(data).length === 0) {
+        return (
+            <div className={styles.noData}>
+                {i18n.t('No data to show for this feature.')}
+            </div>
+        )
+    }
+
+    return (
+        <div className={styles.featureData}>
+            <div className={styles.dataTable}>
+                <table>
+                    <tbody>
+                        {Object.keys(data).map((key) => (
+                            <tr key={key}>
+                                <th>{key}</th>
+                                <td>{data[key]}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+DrawerContent.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
 const FeatureProfile = () => {
     const featureProfile = useSelector((state) => state.featureProfile)
     const dispatch = useDispatch()
 
     const { name, data } = featureProfile
-
-    const getContent = () => {
-        if (Object.keys(data).length === 0) {
-            return (
-                <div className={styles.noData}>
-                    {i18n.t('No data to show for this feature.')}
-                </div>
-            )
-        }
-
-        return (
-            <div className={styles.featureData}>
-                <div className={styles.dataTable}>
-                    <table>
-                        <tbody>
-                            {Object.keys(data).map((key) => (
-                                <tr key={key}>
-                                    <th>{key}</th>
-                                    <td>{data[key]}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        )
-    }
 
     return (
         <Drawer className={styles.drawer}>
@@ -50,7 +55,9 @@ const FeatureProfile = () => {
                     <IconCross24 />
                 </span>
             </div>
-            <div className={styles.content}>{getContent()}</div>
+            <div className={styles.content}>
+                <DrawerContent data={data} />
+            </div>
         </Drawer>
     )
 }
