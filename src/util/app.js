a594f8963087a3dbdcb0eecdc003027de47558e7
braimbault <bruno@dhis2.org>
2024-06-26 15:39:48 +0200

    chore: passing fields to system/info request

--- src/util/app.js
+++ src/util/app.js
@@ -29,6 +29,9 @@ export const appQueries = {
     },
     systemInfo: {
         resource: 'system/info',
+        params: {
+            fields: 'calendar',
+        },
     },
     userSettings: {
         resource: 'userSettings',
