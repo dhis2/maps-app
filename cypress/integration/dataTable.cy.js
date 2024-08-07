90f122b79763b13d802c70df1b214e96c28eaaf7
Jen Jones Arnesen <jennifer@dhis2.org>
2024-08-07 11:32:32 +0200

    chore: try enabling cypress test for event data table

--- cypress/integration/dataTable.cy.js
+++ cypress/integration/dataTable.cy.js
@@ -133,7 +133,7 @@ describe('data table', () => {
         cy.getByDataTest('org-unit-profile').should('be.visible')
     })
 
-    it.skip('opens the data table for an Event layer', () => {
+    it('opens the data table for an Event layer', () => {
         cy.visit('/', EXTENDED_TIMEOUT)
 
         const Layer = new EventLayer()
