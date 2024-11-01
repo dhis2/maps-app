80f9975b4f81268161098d779d828734da86a00c
braimbault <bruno@dhis2.org>
2024-11-01 13:14:52 +0100

    chore: cypress test for event layer popup

--- cypress/integration/layers/eventlayer.cy.js
+++ cypress/integration/layers/eventlayer.cy.js
@@ -52,4 +52,56 @@ context('Event Layers', () => {
         Layer.validateCardTitle('Stage 1 - Repeatable')
         Layer.validateCardItems(['Yes', 'No', 'Not set'])
     })
+
+    it('opens an event popup', () => {
+        Layer.openDialog('Events')
+            .selectProgram('Inpatient morbidity and mortality')
+            .validateStage('Inpatient morbidity and mortality')
+            .selectTab('Style')
+            .selectViewAllEvents()
+            .selectTab('Org Units')
+            .selectOu('Sierra Leone')
+
+        cy.getByDataTest('org-unit-tree-node')
+            .contains('Bo')
+            .parents('[data-test="org-unit-tree-node"]')
+            .first()
+            .within(() => {
+                cy.getByDataTest('org-unit-tree-node-toggle').click()
+            })
+
+        cy.getByDataTest('org-unit-tree-node')
+            .contains('Badjia')
+            .parents('[data-test="org-unit-tree-node"]')
+            .first()
+            .within(() => {
+                cy.getByDataTest('org-unit-tree-node-toggle').click()
+            })
+
+        cy.getByDataTest('org-unit-tree-node').contains('Njandama MCHP').click()
+
+        cy.getByDataTest('layeredit-addbtn').click()
+
+        cy.wait(5000) // eslint-disable-line cypress/no-unnecessary-waiting
+        cy.get('#dhis2-map-container')
+            .findByDataTest('dhis2-uicore-componentcover', EXTENDED_TIMEOUT)
+            .should('not.exist')
+        cy.get('.dhis2-map').click('center')
+
+        cy.get('.maplibregl-popup')
+            .contains('Event location')
+            .should('be.visible')
+        cy.get('.maplibregl-popup')
+            .contains('Organisation unit')
+            .should('be.visible')
+        cy.get('.maplibregl-popup').contains('Event time').should('be.visible')
+
+        cy.get('.maplibregl-popup')
+            .contains('Age in years')
+            .should('be.visible')
+        cy.get('.maplibregl-popup').contains('64').should('be.visible')
+
+        Layer.validateCardTitle('Inpatient morbidity and mortality')
+        Layer.validateCardItems(['Event'])
+    })
 })
