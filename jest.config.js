01a32e95b6f19f41ac20b433c3c35abd20778041
Adel Bensaad <adel@dhis2.org>
2024-11-22 08:24:43 +0000

    chore: remove ReportPortal integration

--- jest.config.js
+++ jest.config.js
@@ -1,48 +1,3 @@
-const reportPortalConfig = [
-    '@reportportal/agent-js-jest',
-    {
-        apiKey: process.env.REPORTPORTAL_API_KEY,
-        endpoint: process.env.REPORTPORTAL_ENDPOINT,
-        project: process.env.REPORTPORTAL_PROJECT,
-        launch: 'maps_app',
-        attributes: [
-            {
-                key: 'dhis2_version',
-                value: 'master',
-            },
-            {
-                key: 'app_name',
-                value: 'maps-app',
-            },
-            {
-                key: 'test_level',
-                value: 'unit/integration',
-            },
-            {
-                key: 'BRANCH_NAME',
-                value: process.env.BRANCH_NAME,
-            },
-            {
-                key: 'CI_BUILD_ID',
-                value: process.env.CI_BUILD_ID,
-            },
-            {
-                key: 'PR_TITLE',
-                value: process.env.PR_TITLE,
-            },
-        ],
-        description: '',
-        debug: false,
-    },
-]
-
-const isDependabotPR = process.env.GITHUB_ACTOR === 'dependabot[bot]'
-const isGithubActionsRun = process.env.CI === 'true'
-const isReportPortalSetup =
-    process.env.REPORTPORTAL_API_KEY !== undefined &&
-    process.env.REPORTPORTAL_ENDPOINT !== undefined &&
-    process.env.REPORTPORTAL_PROJECT !== undefined
-
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/config/testSetup.js'],
     collectCoverageFrom: ['src/**/*.js'],
@@ -57,10 +12,5 @@ module.exports = {
     snapshotSerializers: ['enzyme-to-json/serializer'],
 
     testRunner: 'jest-circus/runner',
-    reporters: [
-        'default',
-        ...(isGithubActionsRun && isReportPortalSetup && !isDependabotPR
-            ? [reportPortalConfig]
-            : []),
-    ],
+    reporters: ['default'],
 }
