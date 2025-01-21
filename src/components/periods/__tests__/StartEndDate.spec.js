d4268605572e87c37e2503cbf873587faa2c190c
braimbault <bruno@dhis2.org>
2025-01-21 15:22:59 +0100

    chore: fix lint error

--- src/components/periods/__tests__/StartEndDate.spec.js
+++ src/components/periods/__tests__/StartEndDate.spec.js
@@ -1,10 +1,10 @@
 import { mount } from 'enzyme'
 import React from 'react'
 import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
+import configureMockStore from 'redux-mock-store'
 import StartEndDate from '../StartEndDate.js'
 
-const mockStore = configureStore()
+const mockStore = configureMockStore()
 
 describe('StartEndDate Component', () => {
     let store
--- src/components/periods/__tests__/Timeline.spec.js
+++ src/components/periods/__tests__/Timeline.spec.js
@@ -1,10 +1,10 @@
 import { mount } from 'enzyme'
 import React from 'react'
 import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
+import configureMockStore from 'redux-mock-store'
 import Timeline from '../Timeline.js'
 
-const mockStore = configureStore()
+const mockStore = configureMockStore()
 
 describe('Timeline', () => {
     const store = mockStore({
