7934c7fbfed14a155693f4f9b23b1ef88619ddc1
Kai Vandivier <kpvandivier@gmail.com>
2025-02-06 15:23:32 +0100

    chore: update test dependencies

--- config/testSetup.js
+++ config/testSetup.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@cfaester/enzyme-adapter-react-18'
 import React from 'react'
 
 // https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server
