1e48d0d1925a90f930ebd381f56736e93dc952d0
HendrikThePendric <hendrik@dhis2.org>
2025-03-12 15:50:12 +0100

    chore: add e2e test to assert push-analytics prerequisites

---
+++ cypress/integration/pushAnalytics.cy.js
@@ -0,0 +1,18 @@
+describe('push-analytics', () => {
+    it(['>=41'], 'has a push-analytics.json file', () => {
+        cy.visit('/')
+        cy.request('push-analytics.json').as('file')
+
+        cy.get('@file').should((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body).to.have.property('version')
+            expect(response.body).to.have.property('showVisualization')
+            expect(response.body).to.have.property('triggerDownload')
+            expect(response.body).to.have.property('obtainDownloadArtifact')
+            expect(response.body).to.have.property('clearVisualization')
+        })
+    })
+    /* The Push Analytics service produces a map artefact using the exact
+     * same process as is veriefied in `mapDownload.cy.js`, so no further
+     * tests are required here. */
+})
