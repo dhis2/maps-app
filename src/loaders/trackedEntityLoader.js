09bbb1285436cb66dcbec1af94a6736c44bb6161
braimbault <bruno@dhis2.org>
2025-05-06 11:21:13 +0200

    fix: introduce feature toggling

--- src/loaders/trackedEntityLoader.js
+++ src/loaders/trackedEntityLoader.js
@@ -167,9 +167,8 @@ const trackedEntityLoader = async ({ config, engine, serverVersion }) => {
     let boolFollowUp = undefined
 
     if (program && programStatus) {
-        explanation = `${i18n.t('Program status')}: ${
-            getProgramStatuses().find((s) => s.id === programStatus).name
-        }`
+        explanation = `${i18n.t('Program status')}: ${getProgramStatuses().find((s) => s.id === programStatus).name
+            }`
     }
 
     if (program && followUp !== undefined) {
