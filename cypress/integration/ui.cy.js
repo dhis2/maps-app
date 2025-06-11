62398abf32a582f22a4685130b4c690041dda16f
Jen Jones Arnesen <jennifer@dhis2.org>
2025-06-11 12:07:39 +0200

    test: add cypress test

--- cypress/integration/ui.cy.js
+++ cypress/integration/ui.cy.js
@@ -1,3 +1,5 @@
+import { FacilityLayer } from '../elements/facility_layer.js'
+import { ThematicLayer } from '../elements/thematic_layer.js'
 import { EXTENDED_TIMEOUT } from '../support/util.js'
 
 const map = {
@@ -35,5 +37,55 @@ describe('ui', () => {
         cy.getByDataTest('add-layer-button').should('be.visible')
         cy.getByDataTest('layers-panel').should('be.visible')
         cy.getByDataTest(map.cardSelector).should('be.visible')
+    })
+
+    it('adds layersSorting class to body when dragging a layer', () => {
+        const ThemLayer = new ThematicLayer()
+        const FacLayer = new FacilityLayer()
+        // 1. Create a map and add a thematic layer and a facility layer
+        cy.visit('/')
+        cy.get('canvas').should('be.visible')
+
+        // Add thematic layer
+        ThemLayer.openDialog('Thematic')
+            .selectIndicatorGroup('HIV')
+            .selectIndicator('VCCT post-test counselling rate')
+            .selectTab('Org Units')
+            .selectOu('Sierra Leone')
+            .addToMap()
+
+        // Add facility layer
+        FacLayer.openDialog('Facilities')
+            .selectTab('Organisation Units')
+            .selectOu('Bo')
+            .selectOuLevel('Facility')
+            .addToMap()
+
+        cy.document().then((doc) => {
+            expect([...doc.body.classList]).not.to.include('layersSorting')
+        })
+        // 2. Start dragging one of the layers (simulate drag handle mousedown and move)
+        // eslint-disable-next-line cypress/unsafe-to-chain-command
+        cy.getByDataTest('sortable-handle')
+            .first()
+            .trigger('mousedown', { button: 0, force: true })
+            .trigger('mousemove', { clientY: 100, force: true })
+
+        // 3. Check that document.body has class 'layersSorting'
+        cy.document().then((doc) => {
+            expect([...doc.body.classList]).to.include('layersSorting')
+        })
+
+        // 4. End the drag (mouseup) and assert the class is removed
+        cy.getByDataTest('sortable-handle')
+            .first()
+            .trigger('mouseup', { force: true })
+
+        // Wait for the timeout in onSortEnd (100ms)
+        cy.wait(150) // eslint-disable-line cypress/no-unnecessary-waiting
+
+        cy.document().then((doc) => {
+            expect([...doc.body.classList]).not.to.include('layersSorting')
+        })
     })
 })
