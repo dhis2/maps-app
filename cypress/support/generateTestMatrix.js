7888a670753b902177412aa9fc77395fb88c3a35
braimbault <bruno@dhis2.org>
2025-09-29 16:54:38 +0200

    chore: add option to filter cypress test files

--- cypress/support/generateTestMatrix.js
+++ cypress/support/generateTestMatrix.js
@@ -1,14 +1,47 @@
 const fs = require('fs')
 const path = require('path')
 
-const getAllFiles = (dirPath, arrayOfFiles = []) => {
+const excludedFiles = new Set([
+    'basemaps.cy.js',
+    'dataDownload.cy.js',
+    'dataTable.cy.js',
+    'fetcherrors.cy.js',
+    'filemenu.cy.js',
+    'interpretations.cy.js',
+    'keyboard.cy.js',
+    'manageLayerSources.cy.js',
+    'mapDownload.cy.js',
+    'orgUnitInfo.cy.js',
+    'plugin.cy.js',
+    'pushAnalytics.cy.js',
+    'requests.cy.js',
+    'routes.cy.js',
+    'systemsettings.cy.js',
+    'ui.cy.js',
+    'usersettings.cy.js',
+    'eventlayer.cy.js',
+    'multilayers.cy.js',
+    'externallayer.cy.js',
+    'orgunitlayer.cy.js',
+    'facilitylayer.cy.js',
+    'thematiclayer.cy.js',
+    'geojsonlayer.cy.js',
+    'trackedentitylayer.cy.js',
+])
+
+const filterFn = (file) => !excludedFiles.has(file)
+
+const getAllFiles = (dirPath, arrayOfFiles = [], filterFn = () => true) => {
     const files = fs.readdirSync(dirPath)
 
     files.forEach((file) => {
-        if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
-            arrayOfFiles = getAllFiles(path.join(dirPath, file), arrayOfFiles)
-        } else if (path.extname(file) === '.js') {
-            arrayOfFiles.push(path.join(dirPath, file))
+        const fullPath = path.join(dirPath, file)
+        const stats = fs.statSync(fullPath)
+
+        if (stats.isDirectory()) {
+            arrayOfFiles = getAllFiles(fullPath, arrayOfFiles, filterFn)
+        } else if (path.extname(file) === '.js' && filterFn(file, fullPath)) {
+            arrayOfFiles.push(fullPath)
         }
     })
 
@@ -29,7 +62,7 @@ const createGroups = (files, numberOfGroups = 5) => {
 }
 
 const cypressSpecsPath = './cypress/integration'
-const specs = getAllFiles(cypressSpecsPath)
+const specs = getAllFiles(cypressSpecsPath, [], filterFn)
 const groupedSpecs = createGroups(specs)
 
 console.log(JSON.stringify(groupedSpecs))
