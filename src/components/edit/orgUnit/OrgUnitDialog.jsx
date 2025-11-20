9e819e95b3ca8d805f08b165effbeb7515aecf94
Bruno Raimbault <bruno@dhis2.org>
2025-11-20 17:35:43 +0100

    chore: refactor OrgUnitDialog to functional component

--- src/components/edit/orgUnit/OrgUnitDialog.jsx
+++ src/components/edit/orgUnit/OrgUnitDialog.jsx
@@ -1,7 +1,7 @@
 import i18n from '@dhis2/d2-i18n'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect, useState, useCallback } from 'react'
+import { useDispatch } from 'react-redux'
 import {
     setRadiusLow,
     setOrganisationUnitColor,
@@ -20,122 +20,112 @@ import OrgUnitSelect from '../../orgunits/OrgUnitSelect.jsx'
 import Labels from '../shared/Labels.jsx'
 import styles from '../styles/LayerDialog.module.css'
 
-class OrgUnitDialog extends Component {
-    static propTypes = {
-        setOrganisationUnitColor: PropTypes.func.isRequired,
-        setRadiusLow: PropTypes.func.isRequired,
-        validateLayer: PropTypes.bool.isRequired,
-        onLayerValidation: PropTypes.func.isRequired,
-        organisationUnitColor: PropTypes.string,
-        radiusLow: PropTypes.number,
-        rows: PropTypes.array,
-    }
+const OrgUnitDialog = ({
+    validateLayer,
+    onLayerValidation,
+    organisationUnitColor,
+    radiusLow,
+    rows,
+}) => {
+    const dispatch = useDispatch()
 
-    state = {
-        tab: 'orgunits',
-    }
+    const [tab, setTab] = useState('orgunits')
+    const [orgUnitsError, setOrgUnitsError] = useState(null)
 
-    componentDidUpdate(prev) {
-        const { validateLayer, onLayerValidation } = this.props
-
-        if (validateLayer && validateLayer !== prev.validateLayer) {
-            onLayerValidation(this.validate())
+    // --------------------------
+    // Validation (was componentDidUpdate)
+    // --------------------------
+    useEffect(() => {
+        if (validateLayer) {
+            onLayerValidation(validate())
         }
-    }
+    }, [validateLayer])
 
-    render() {
-        const {
-            radiusLow,
-            organisationUnitColor,
-            setOrganisationUnitColor,
-            setRadiusLow,
-        } = this.props
-
-        const { tab, orgUnitsError } = this.state
-
-        return (
-            <div className={styles.content} data-test="orgunitdialog">
-                <Tabs value={tab} onChange={(tab) => this.setState({ tab })}>
-                    <Tab value="orgunits">{i18n.t('Organisation Units')}</Tab>
-                    <Tab value="style">{i18n.t('Style')}</Tab>
-                </Tabs>
-                <div className={styles.tabContent}>
-                    {tab === 'orgunits' && (
-                        <OrgUnitSelect warning={orgUnitsError} />
-                    )}
-                    {tab === 'style' && (
-                        <div
-                            className={styles.flexColumnFlow}
-                            data-test="orgunitdialog-styletab"
-                        >
-                            <div className={styles.flexColumn}>
-                                <Labels />
-                                <ColorPicker
-                                    label={i18n.t('Boundary color')}
-                                    color={
-                                        organisationUnitColor || ORG_UNIT_COLOR
-                                    }
-                                    onChange={setOrganisationUnitColor}
-                                    className={styles.narrowField}
-                                />
-                                <NumberField
-                                    label={i18n.t('Point radius')}
-                                    min={MIN_RADIUS}
-                                    max={MAX_RADIUS}
-                                    value={
-                                        radiusLow !== undefined
-                                            ? radiusLow
-                                            : ORG_UNIT_RADIUS
-                                    }
-                                    onChange={setRadiusLow}
-                                    className={styles.narrowFieldIcon}
-                                />
-                            </div>
-                            <div className={styles.flexColumn}>
-                                <StyleByGroupSet
-                                    defaultStyleType={STYLE_TYPE_COLOR}
-                                />
-                            </div>
-                        </div>
-                    )}
-                </div>
-            </div>
-        )
-    }
-
-    // TODO: Add to parent class?
-    setErrorState(key, message, tab) {
-        this.setState({
-            [key]: message,
-            tab,
-        })
-
+    // --------------------------
+    // Validation handler
+    // --------------------------
+    const setErrorState = useCallback((message, tabName) => {
+        setOrgUnitsError(message)
+        setTab(tabName)
         return false
-    }
+    }, [])
 
-    validate() {
-        const { rows } = this.props
-
+    const validate = useCallback(() => {
         if (!getOrgUnitsFromRows(rows).length) {
-            return this.setErrorState(
-                'orgUnitsError',
+            return setErrorState(
                 i18n.t('No organisation units are selected'),
                 'orgunits'
             )
         }
-
         return true
-    }
+    }, [rows, setErrorState])
+
+    // --------------------------
+    // Render
+    // --------------------------
+    return (
+        <div className={styles.content} data-test="orgunitdialog">
+            <Tabs value={tab} onChange={setTab}>
+                <Tab value="orgunits">{i18n.t('Organisation Units')}</Tab>
+                <Tab value="style">{i18n.t('Style')}</Tab>
+            </Tabs>
+
+            <div className={styles.tabContent}>
+                {tab === 'orgunits' && (
+                    <OrgUnitSelect warning={orgUnitsError} />
+                )}
+
+                {tab === 'style' && (
+                    <div
+                        className={styles.flexColumnFlow}
+                        data-test="orgunitdialog-styletab"
+                    >
+                        <div className={styles.flexColumn}>
+                            <Labels />
+                            <ColorPicker
+                                label={i18n.t('Boundary color')}
+                                color={organisationUnitColor || ORG_UNIT_COLOR}
+                                onChange={(color) =>
+                                    dispatch(setOrganisationUnitColor(color))
+                                }
+                                className={styles.narrowField}
+                            />
+                            <NumberField
+                                label={i18n.t('Point radius')}
+                                min={MIN_RADIUS}
+                                max={MAX_RADIUS}
+                                value={
+                                    radiusLow !== undefined
+                                        ? radiusLow
+                                        : ORG_UNIT_RADIUS
+                                }
+                                onChange={(value) =>
+                                    dispatch(setRadiusLow(value))
+                                }
+                                className={styles.narrowFieldIcon}
+                            />
+                        </div>
+
+                        <div className={styles.flexColumn}>
+                            <StyleByGroupSet
+                                defaultStyleType={STYLE_TYPE_COLOR}
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
 
-export default connect(
-    null,
-    {
-        setRadiusLow,
-        setOrganisationUnitColor,
-    },
-    null,
-    {
-        forwardRef: true,
-    }
-)(OrgUnitDialog)
+OrgUnitDialog.propTypes = {
+    setOrganisationUnitColor: PropTypes.func,
+    setRadiusLow: PropTypes.func,
+    validateLayer: PropTypes.bool.isRequired,
+    onLayerValidation: PropTypes.func.isRequired,
+    organisationUnitColor: PropTypes.string,
+    radiusLow: PropTypes.number,
+    rows: PropTypes.array,
+}
+
+export default OrgUnitDialog
