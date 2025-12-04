f8cf0b0c396fae731efe49152cb2f3ba38524821
Bruno Raimbault <bruno@dhis2.org>
2025-12-04 11:19:27 +0100

    feat: allow adding additional thematic layers if a splitview is active

--- src/components/layers/overlays/AddLayerPopover.jsx
+++ src/components/layers/overlays/AddLayerPopover.jsx
@@ -66,7 +66,7 @@ const AddLayerPopover = ({ anchorEl, onClose, onManaging }) => {
                 isSplitView={isSplitView}
                 onLayerSelect={onLayerSelect}
             />
-            <ManageLayerSourcesButton onClick={onManaging} />
+            {!isSplitView && <ManageLayerSourcesButton onClick={onManaging} />}
         </Popover>
     )
 }
