3e4996acf2a0132c524f2164d8b3701a38e020e9
Bruno Raimbault <bruno@dhis2.org>
2026-05-19 13:32:00 +0200

    fix: don't inherit period from single thematic layer when adding new thematic layer [DHIS2-21517]

--- src/components/edit/thematic/ThematicDialog.jsx
+++ src/components/edit/thematic/ThematicDialog.jsx
@@ -119,6 +119,7 @@ const ThematicDialog = ({
                 orgUnits,
                 systemSettings,
                 syncFromOtherLayers,
+                shouldSyncFromOtherLayers,
             })
         )
         // eslint-disable-next-line react-hooks/exhaustive-deps
