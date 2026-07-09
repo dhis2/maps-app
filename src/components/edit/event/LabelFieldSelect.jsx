7d1c42920dcff4c68b8aeba8cd0dff2a5d88f0c8
Bruno Raimbault <bruno@dhis2.org>
2026-07-09 10:52:00 +0200

    chore: create a EventDataItemsProvider for useEventDataItems

--- src/components/edit/event/LabelFieldSelect.jsx
+++ src/components/edit/event/LabelFieldSelect.jsx
@@ -1,22 +1,18 @@
 import i18n from '@dhis2/d2-i18n'
-import PropTypes from 'prop-types'
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { setLabelDataItem } from '../../../actions/layerEdit.js'
-import { useEventDataItems } from '../../../hooks/useEventDataItems.js'
+import { useEventDataItems } from '../../dataItem/EventDataItemsProvider.jsx'
 import Labels from '../shared/Labels.jsx'
 
 const ITEM_NONE = { id: 'none', name: i18n.t('None') }
 
-const LabelFieldSelect = ({ program, programStage }) => {
+const LabelFieldSelect = () => {
     const labelDataItemId = useSelector(
         (state) => state.layerEdit.labelDataItem?.id
     )
     const dispatch = useDispatch()
-    const { eventDataItems } = useEventDataItems({
-        programId: program?.id,
-        programStageId: programStage?.id,
-    })
+    const { eventDataItems } = useEventDataItems()
 
     if (eventDataItems === null) {
         return null
@@ -35,15 +31,6 @@ const LabelFieldSelect = ({ program, programStage }) => {
             }
         />
     )
-}
-
-LabelFieldSelect.propTypes = {
-    program: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-    }),
-    programStage: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-    }),
 }
 
 export default LabelFieldSelect
