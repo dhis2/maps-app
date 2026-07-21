a3ac54fef4774e2e9ca8ba5999001f87d03f5ee1
Bruno Raimbault <bruno@dhis2.org>
2026-07-21 13:22:22 +0200

    fix: datatable performance optimisation

--- src/components/datatable/BottomPanel.jsx
+++ src/components/datatable/BottomPanel.jsx
@@ -238,19 +238,17 @@ const BottomPanel = () => {
                 <span className={styles.divider} />
                 <CloseControl onClick={onCloseDataTable} />
             </div>
-            {!isCollapsed && (
-                <div className={styles.tableContainer}>
-                    <ErrorBoundary>
-                        <DataTable
-                            availableWidth={panelWidth}
-                            onCountChange={onCountChange}
-                            onHeadersChange={onHeadersChange}
-                            globalSearch={globalSearch}
-                            onClearFilters={onClearFilters}
-                        />
-                    </ErrorBoundary>
-                </div>
-            )}
+            <div className={styles.tableContainer}>
+                <ErrorBoundary>
+                    <DataTable
+                        availableWidth={panelWidth}
+                        onCountChange={onCountChange}
+                        onHeadersChange={onHeadersChange}
+                        globalSearch={globalSearch}
+                        onClearFilters={onClearFilters}
+                    />
+                </ErrorBoundary>
+            </div>
         </div>
     )
 }
