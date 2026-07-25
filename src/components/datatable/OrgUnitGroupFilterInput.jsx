e4c430d957235312c54e2c0e60155a15e2f1de5c
Bruno Raimbault <bruno@dhis2.org>
2026-07-25 15:14:40 +0200

    chore: GroupFilterPopover refactor

--- src/components/datatable/OrgUnitGroupFilterInput.jsx
+++ src/components/datatable/OrgUnitGroupFilterInput.jsx
@@ -1,15 +1,7 @@
 import i18n from '@dhis2/d2-i18n'
-import {
-    Input,
-    IconChevronRight16,
-    IconChevronDown16,
-    IconFilter16,
-} from '@dhis2/ui'
-import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React, { useCallback, useMemo, useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { Virtuoso } from 'react-virtuoso'
 import { setDataFilter, clearDataFilter } from '../../actions/dataFilters.js'
 import {
     SENTINEL_ANY_VALUE,
@@ -19,12 +11,9 @@ import {
 import useOrgUnitAncestorNames from '../../hooks/useOrgUnitAncestorNames.js'
 import { isOrgUnitGroupFilter } from '../../util/filter.js'
 import {
-    OPTION_ROW_HEIGHT,
-    MAX_LIST_HEIGHT,
     getCyclicIndex,
     getDisplayValue,
     toOptionIndex,
-    toHighlightedIndex,
 } from '../../util/filterInput.js'
 import { toggleAnyValue } from '../../util/filterSelection.js'
 import {
@@ -39,23 +28,15 @@ import {
     flattenVisibleNodes,
     nodeMatchesOrHasMatch,
 } from '../../util/prefixTree.js'
-import Checkbox from '../core/Checkbox.jsx'
-import {
-    FilterDropdownPopover,
-    getDropdownPlacement,
-} from './FilterDropdownPopover.jsx'
-import FilterHelpTooltip from './FilterHelpTooltip.jsx'
-import styles from './styles/FilterInput.module.css'
+import { getDropdownPlacement } from './FilterDropdownPopover.jsx'
+import GroupFilterPopover from './GroupFilterPopover.jsx'
 
-const ORG_UNIT_GROUP_POPOVER_WIDTH = 220
-const HELP_HEIGHT = 56
 const HELP_CONTENT = (
     <div>
         <div>{i18n.t('Select a country, region, district or facility')}</div>
         <div>{i18n.t('to match the rows under it, or type to search')}</div>
     </div>
 )
-const INDENT_PX = 16
 
 const getAppliedString = (filterValue) => {
     if (isOrgUnitGroupFilter(filterValue)) {
@@ -340,198 +321,40 @@ const OrgUnitGroupFilterInput = ({
     })
 
     return (
-        <div className={styles.filterTrigger} ref={anchorRef}>
-            <FilterHelpTooltip
-                content={HELP_CONTENT}
-                placement={tooltipPlacement}
-                estimatedHeight={HELP_HEIGHT}
-                dataTest="data-table-filter-help"
-            >
-                <Input
-                    dense
-                    clearable
-                    dataTest={`data-table-column-filter-search-${name}`}
-                    placeholder={i18n.t('Search')}
-                    value={displayValue}
-                    onFocus={() => {
-                        if (!isOpen) {
-                            openPopover()
-                        }
-                    }}
-                    onChange={onSearchChange}
-                    onKeyDown={onSearchKeyDown}
-                />
-            </FilterHelpTooltip>
-            {isOpen && (
-                <FilterDropdownPopover
-                    reference={anchorRef}
-                    placement={dropdownPlacement}
-                    onClickOutside={closePopover}
-                    className={cx(
-                        styles.dropdownPopper,
-                        dropdownSide === 'top' && styles.dropdownPopperAbove
-                    )}
-                >
-                    <div
-                        className={cx(styles.searchableFilterPopover, {
-                            [styles.reversedOrder]: dropdownSide === 'top',
-                        })}
-                        style={{ width: `${ORG_UNIT_GROUP_POPOVER_WIDTH}px` }}
-                    >
-                        {showCustomFilterRow && (
-                            <button
-                                type="button"
-                                className={cx(styles.customFilterRow, {
-                                    [styles.highlighted]:
-                                        highlightedIndex === 0,
-                                })}
-                                data-test={`data-table-column-filter-custom-${name}`}
-                                onClick={() => {
-                                    applyCustomFilter(searchText.trim())
-                                    closePopover()
-                                }}
-                            >
-                                <IconFilter16 />
-                                <span className={styles.customFilterTag}>
-                                    {i18n.t('Select matches')}
-                                </span>
-                                <span className={styles.customFilterExpr}>
-                                    {searchText.trim()}
-                                </span>
-                            </button>
-                        )}
-                        <div className={styles.pinnedOptions}>
-                            <Checkbox
-                                label={i18n.t('Any value')}
-                                checked={anyValueActive}
-                                onChange={onToggleAnyValue}
-                                className={cx(
-                                    styles.specialOption,
-                                    styles.denseCheckbox
-                                )}
-                                dataTest={`data-table-column-filter-any-${name}`}
-                            />
-                            {hasNotSetOption && (
-                                <Checkbox
-                                    label={i18n.t('No value')}
-                                    checked={notSetActive}
-                                    onChange={onToggleNotSet}
-                                    className={cx(
-                                        styles.specialOption,
-                                        styles.denseCheckbox
-                                    )}
-                                    dataTest={`data-table-column-filter-novalue-${name}`}
-                                />
-                            )}
-                        </div>
-                        <div className={styles.multiSelectPopover}>
-                            {!showCustomFilterRow &&
-                                visibleNodes.length === 0 && (
-                                    <div className={styles.noResults}>
-                                        {i18n.t('No matches')}
-                                    </div>
-                                )}
-                            {visibleNodes.length > 0 && (
-                                <Virtuoso
-                                    ref={listRef}
-                                    style={{
-                                        height: Math.min(
-                                            visibleNodes.length *
-                                                OPTION_ROW_HEIGHT,
-                                            MAX_LIST_HEIGHT
-                                        ),
-                                    }}
-                                    increaseViewportBy={{
-                                        top: 0,
-                                        bottom: OPTION_ROW_HEIGHT * 2,
-                                    }}
-                                    data={visibleNodes}
-                                    fixedItemHeight={OPTION_ROW_HEIGHT}
-                                    computeItemKey={(_, { node }) => node.key}
-                                    itemContent={(index, { node, depth }) => {
-                                        const state = checkStateFor(node)
-                                        const checked = state === 'checked'
-                                        const indeterminate =
-                                            state === 'indeterminate'
-                                        const isExpanded =
-                                            effectiveExpanded.has(node.key)
-                                        const label = formatOrgUnitNodeLabel(
-                                            node,
-                                            idToName
-                                        )
-                                        return (
-                                            <div
-                                                className={styles.treeRow}
-                                                style={{
-                                                    paddingLeft:
-                                                        depth * INDENT_PX,
-                                                }}
-                                            >
-                                                {node.children.length > 0 ? (
-                                                    <button
-                                                        type="button"
-                                                        className={
-                                                            styles.expandButton
-                                                        }
-                                                        onClick={() =>
-                                                            onToggleExpand(
-                                                                node.key
-                                                            )
-                                                        }
-                                                        aria-label={
-                                                            isExpanded
-                                                                ? i18n.t(
-                                                                      'Collapse {{label}}',
-                                                                      { label }
-                                                                  )
-                                                                : i18n.t(
-                                                                      'Expand {{label}}',
-                                                                      { label }
-                                                                  )
-                                                        }
-                                                    >
-                                                        {isExpanded ? (
-                                                            <IconChevronDown16 />
-                                                        ) : (
-                                                            <IconChevronRight16 />
-                                                        )}
-                                                    </button>
-                                                ) : (
-                                                    <span
-                                                        className={
-                                                            styles.expandButtonPlaceholder
-                                                        }
-                                                    />
-                                                )}
-                                                <Checkbox
-                                                    label={label}
-                                                    checked={checked}
-                                                    indeterminate={
-                                                        indeterminate
-                                                    }
-                                                    onChange={() =>
-                                                        onToggleNode(node)
-                                                    }
-                                                    className={cx(
-                                                        styles.denseCheckbox,
-                                                        highlightedIndex ===
-                                                            toHighlightedIndex(
-                                                                index,
-                                                                showCustomFilterRow
-                                                            ) &&
-                                                            styles.highlighted
-                                                    )}
-                                                />
-                                            </div>
-                                        )
-                                    }}
-                                />
-                            )}
-                        </div>
-                    </div>
-                </FilterDropdownPopover>
-            )}
-        </div>
+        <GroupFilterPopover
+            name={name}
+            helpContent={HELP_CONTENT}
+            customFilterTag={i18n.t('Select matches')}
+            formatLabel={(node) => formatOrgUnitNodeLabel(node, idToName)}
+            anchorRef={anchorRef}
+            listRef={listRef}
+            dropdownPlacement={dropdownPlacement}
+            dropdownSide={dropdownSide}
+            tooltipPlacement={tooltipPlacement}
+            isOpen={isOpen}
+            searchText={searchText}
+            highlightedIndex={highlightedIndex}
+            displayValue={displayValue}
+            visibleNodes={visibleNodes}
+            showCustomFilterRow={showCustomFilterRow}
+            anyValueActive={anyValueActive}
+            notSetActive={notSetActive}
+            hasNotSetOption={hasNotSetOption}
+            effectiveExpanded={effectiveExpanded}
+            checkStateFor={checkStateFor}
+            openPopover={openPopover}
+            closePopover={closePopover}
+            onSearchChange={onSearchChange}
+            onSearchKeyDown={onSearchKeyDown}
+            onApplyCustomFilterClick={() => {
+                applyCustomFilter(searchText.trim())
+                closePopover()
+            }}
+            onToggleExpand={onToggleExpand}
+            onToggleNode={onToggleNode}
+            onToggleAnyValue={onToggleAnyValue}
+            onToggleNotSet={onToggleNotSet}
+        />
     )
 }
 
